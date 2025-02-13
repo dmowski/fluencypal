fc88d860d4cd6293d8d1a55e5bbc930a55fa0ade
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-02-13 15:53:47 +0100

    TMP

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Privacy Policy</h2>
+    </main>
+  );
+}
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Terms of Use</h2>
+    </main>
+  );
+}
