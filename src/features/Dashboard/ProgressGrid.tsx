2b1f1f90ed475ee1be1e2ae723d13771b2d4a97a
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-02-22 16:43:18 +0100

    progress grid

--- src/features/Dashboard/ProgressGrid.tsx
+++ src/features/Dashboard/ProgressGrid.tsx
@@ -2,59 +2,57 @@ import React from "react";
 import { Box, Stack, Tooltip, Typography } from "@mui/material";
 import dayjs from "dayjs";
 
-const colorMap = [
-  "rgba(5, 172, 255, 0.03)",
-  "rgba(5, 172, 255, 0.3)",
-  "rgba(5, 172, 255, 0.8)",
-  "rgba(5, 172, 255, 1)",
-];
+const colorMap = ["#0a131c", "#0c3156", "#104982", "#1f8abd"];
 
-interface ProgressGridProps {
+interface MonthProgressBlockProps {
+  month: number;
   currentDateTimeStamp: number;
-  startDateTimeStamp: number;
+  year: number;
   getDateStat: (data: string) => number;
+  daySize: number;
 }
 
-export const ProgressGrid: React.FC<ProgressGridProps> = ({
-  startDateTimeStamp,
+export const MonthProgressBlock: React.FC<MonthProgressBlockProps> = ({
+  month,
+  year,
   currentDateTimeStamp,
   getDateStat,
+  daySize,
 }) => {
-  const daysToShow = 365;
-  const startDate = dayjs(startDateTimeStamp);
-  // endDate - startDate + daysToShow
-  const endDate = startDate.add(daysToShow - 1, "day");
+  const daysInMonth = dayjs().month(month).year(year).daysInMonth();
 
-  const days = Array.from({ length: endDate.diff(startDate, "day") + 1 }, (_, i) =>
-    startDate.add(i, "day")
+  const startDate = dayjs().year(year).month(month).date(1);
+
+  const thisMonthDays = Array.from({ length: daysInMonth }, (_, i) => startDate.add(i, "day"));
+  const weekDayFirstDay = startDate.day() === 0 ? 6 : startDate.day() - 1;
+  const needGhostDays = weekDayFirstDay;
+  const ghostBeforeDays = Array.from({ length: needGhostDays }, (_, i) =>
+    startDate.subtract(i + 1, "day")
   );
+
+  const days = [...ghostBeforeDays.reverse(), ...thisMonthDays];
+
   const currentDayString = dayjs(currentDateTimeStamp).format("DD.MM.YYYY");
-  const progressWidth = "1292px";
 
-  // Generate month labels from startDateTimeStamp to currentDateTimeStamp
-  const months = Array.from({ length: endDate.diff(startDate, "month") + 1 }, (_, i) =>
-    startDate.add(i, "month").format("MMM")
-  );
+  const monthTitle = startDate.format("MMM");
 
   return (
     <Stack
       sx={{
-        position: "relative",
-        width: "100%",
-        height: "170px",
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: needGhostDays > 0 ? `-${daySize + 9}px` : "-7px",
+        width: "max-content",
+        gap: "6px",
       }}
     >
       <Stack
         sx={{
-          height: "140px",
-          width: progressWidth,
-          borderRadius: "6px",
+          height: `${(daySize + 4) * 7}px`,
+          width: "max-content",
           padding: "2px",
           flexWrap: "wrap",
-          gap: "8px 1px",
-          position: "absolute",
-          top: "0",
-          left: "0",
+          gap: "10px 7px",
         }}
       >
         {days.map((date, index) => {
@@ -81,42 +79,84 @@ export const ProgressGrid: React.FC<ProgressGridProps> = ({
             >
               <Box
                 sx={{
-                  width: 12,
-                  height: 12,
+                  width: 17,
+                  height: 17,
                   bgcolor: colorMap[level],
                   borderRadius: "2px",
                   boxSizing: "border-box",
                   boxShadow: isToday
-                    ? "0 0 0 1px #fff"
+                    ? "0 0 0 1px #c2c2c2"
                     : isFuture
-                      ? "0 0 0 1px rgba(255, 255, 255, 0.1)"
-                      : "0 0 0 1px rgba(255, 255, 255, 0.1)",
+                      ? "0 0 0 1px rgba(55, 55, 55, 1)"
+                      : "0 0 0 1px rgba(55, 55, 55, 1)",
                 }}
               />
             </Tooltip>
           );
         })}
       </Stack>
+      <Typography
+        variant="caption"
+        sx={{
+          opacity: 0.5,
+        }}
+      >
+        {monthTitle}
+      </Typography>
+    </Stack>
+  );
+};
+
+interface ProgressGridProps {
+  currentDateTimeStamp: number;
+  startDateTimeStamp: number;
+  getDateStat: (data: string) => number;
+}
+
+export const ProgressGrid: React.FC<ProgressGridProps> = ({
+  startDateTimeStamp,
+  currentDateTimeStamp,
+  getDateStat,
+}) => {
+  const startDate = dayjs(startDateTimeStamp);
+  const endDate = dayjs(currentDateTimeStamp).add(12, "month");
+
+  const monthsAndYears: { month: number; year: number }[] = [];
+  let currentDate = startDate;
+  do {
+    monthsAndYears.push({
+      month: currentDate.month(),
+      year: currentDate.year(),
+    });
+    currentDate = currentDate.add(1, "month");
+  } while (currentDate.isBefore(endDate));
+
+  return (
+    <Stack
+      sx={{
+        position: "relative",
+        width: "100%",
+        maxWidth: "1294px",
+        overflowX: "hidden",
+      }}
+    >
       <Stack
         sx={{
           flexDirection: "row",
-          alignItems: "center",
-          height: "20px",
-          width: progressWidth,
-          borderRadius: "6px",
-          padding: "2px 10px",
-          boxSizing: "border-box",
-          gap: "3px",
-          position: "absolute",
-          bottom: "0",
-          left: "0",
-          justifyContent: "space-between",
+          width: "max-content",
+          gap: "10px",
+          padding: "0px 0 20px 30px",
         }}
       >
-        {months.map((month, index) => (
-          <Typography key={index} variant="caption" sx={{ textAlign: "center", opacity: 0.6 }}>
-            {month}
-          </Typography>
+        {monthsAndYears.map((date, index) => (
+          <MonthProgressBlock
+            daySize={22}
+            key={index}
+            month={date.month}
+            year={date.year}
+            currentDateTimeStamp={currentDateTimeStamp}
+            getDateStat={getDateStat}
+          />
         ))}
       </Stack>
     </Stack>
