204ddc80f162cd8a9bdb188518941f1f2b63f4a8
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-02-25 13:03:55 +0100

    example

---
+++ src/hookExample.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { createContext, useContext, ReactNode, JSX } from "react";
+
+interface NameHookContextType {
+  loading: boolean;
+}
+
+const NameHookContext = createContext<NameHookContextType | null>(null);
+
+function useProvideNameHook(): NameHookContextType {
+  return {
+    loading: false,
+  };
+}
+
+export function NameHookProvider({ children }: { children: ReactNode }): JSX.Element {
+  const hook = useProvideNameHook();
+  return <NameHookContext.Provider value={hook}>{children}</NameHookContext.Provider>;
+}
+
+export const useNameHook = (): NameHookContextType => {
+  const context = useContext(NameHookContext);
+  if (!context) {
+    throw new Error("useNameHook must be used within a NameHookProvider");
+  }
+  return context;
+};
