4902f8100965b296d35799ab65729adb9674182d
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-02 17:05:45 +0100

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
       </head>
       <body className={roboto.variable}>
         <ThemeProvider theme={theme}>
