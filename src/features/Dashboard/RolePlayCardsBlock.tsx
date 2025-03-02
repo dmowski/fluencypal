624e994bf5ca535b94f29f132ddab92ea8c62a06
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-02 03:05:15 +0100

    TMP

--- src/features/Dashboard/RolePlayCardsBlock.tsx
+++ src/features/Dashboard/RolePlayCardsBlock.tsx
@@ -57,6 +57,7 @@ export const RolePlayCardsBlock = () => {
                     textAlign: "left",
                     padding: "0px",
                     boxSizing: "border-box",
+                    color: "#fff",
                     ":hover": {
                       boxShadow:
                         "0px 0px 20px 0px rgba(0,0,0,0.5), 0px 0px 0 1px rgba(255,255,255,0.2)",
