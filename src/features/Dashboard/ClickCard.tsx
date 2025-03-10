c163f9df1de1b1f2bba83daf56766d9e343db765
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-10 20:59:58 +0100

    ref

--- src/features/Dashboard/ClickCard.tsx
+++ src/features/Dashboard/ClickCard.tsx
@@ -23,86 +23,88 @@ export const ClickCard: React.FC<ClickCardProps> = ({
 }) => {
   return (
     <Tooltip title={lockedText} placement="top" arrow>
-      <Stack
-        component={"button"}
-        disabled={!!lockedText}
-        onClick={() => onStart()}
-        sx={{
-          backgroundColor: "transparent",
-          padding: "0",
-          border: "none",
-          display: "flex",
-          alignItems: "flex-start",
-          textAlign: "left",
-          cursor: lockedText ? "not-allowed" : "pointer",
-          width: "100%",
-          height: "max-content",
-          ":hover,:focus": {
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
-            borderRadius: "16px",
-          },
-        }}
-      >
-        <GradientCard
-          padding="22px"
-          strokeWidth="2px"
-          startColor={
-            isDone
-              ? "#fa8500"
-              : lockedText
-                ? "rgba(255, 255, 255, 0.09)"
-                : "rgba(255, 255, 255, 0.2)"
-          }
-          endColor={
-            isDone
-              ? "#05acff"
-              : lockedText
-                ? "rgba(255, 255, 255, 0.09)"
-                : "rgba(255, 255, 255, 0.3)"
-          }
-          backgroundColor={
-            isDone
-              ? "rgba(10, 18, 30, 1)"
-              : lockedText
-                ? "rgba(10, 18, 30, 0.2)"
-                : "rgba(10, 18, 30, 0.6)"
-          }
+      <Stack>
+        <Stack
+          component={"button"}
+          disabled={!!lockedText}
+          onClick={() => onStart()}
+          sx={{
+            backgroundColor: "transparent",
+            padding: "0",
+            border: "none",
+            display: "flex",
+            alignItems: "flex-start",
+            textAlign: "left",
+            cursor: lockedText ? "not-allowed" : "pointer",
+            width: "100%",
+            height: "max-content",
+            ":hover,:focus": {
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              borderRadius: "16px",
+            },
+          }}
         >
-          {lockedText ? <Lock size={"30px"} color="#888" /> : buttonIcon}
-          <Stack
-            sx={{
-              gap: "16px",
-              alignItems: "flex-start",
-            }}
+          <GradientCard
+            padding="22px"
+            strokeWidth="2px"
+            startColor={
+              isDone
+                ? "#fa8500"
+                : lockedText
+                  ? "rgba(255, 255, 255, 0.09)"
+                  : "rgba(255, 255, 255, 0.2)"
+            }
+            endColor={
+              isDone
+                ? "#05acff"
+                : lockedText
+                  ? "rgba(255, 255, 255, 0.09)"
+                  : "rgba(255, 255, 255, 0.3)"
+            }
+            backgroundColor={
+              isDone
+                ? "rgba(10, 18, 30, 1)"
+                : lockedText
+                  ? "rgba(10, 18, 30, 0.2)"
+                  : "rgba(10, 18, 30, 0.6)"
+            }
           >
+            {lockedText ? <Lock size={"30px"} color="#888" /> : buttonIcon}
             <Stack
               sx={{
-                position: "absolute",
-                top: "16px",
-                right: "16px",
+                gap: "16px",
+                alignItems: "flex-start",
               }}
             >
-              {isDone ? <BadgeCheck strokeWidth={"2px"} color="#558fdb" size={"24px"} /> : null}
-            </Stack>
-            <Stack>
-              <Typography
+              <Stack
                 sx={{
-                  opacity: lockedText ? 0.6 : 1,
+                  position: "absolute",
+                  top: "16px",
+                  right: "16px",
                 }}
               >
-                {title}
-              </Typography>
-              <Typography
-                variant="caption"
-                sx={{
-                  opacity: lockedText ? 0.3 : 0.7,
-                }}
-              >
-                {subTitle}
-              </Typography>
+                {isDone ? <BadgeCheck strokeWidth={"2px"} color="#558fdb" size={"24px"} /> : null}
+              </Stack>
+              <Stack>
+                <Typography
+                  sx={{
+                    opacity: lockedText ? 0.6 : 1,
+                  }}
+                >
+                  {title}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{
+                    opacity: lockedText ? 0.3 : 0.7,
+                  }}
+                >
+                  {subTitle}
+                </Typography>
+              </Stack>
             </Stack>
-          </Stack>
-        </GradientCard>
+          </GradientCard>
+        </Stack>
       </Stack>
     </Tooltip>
   );
