a9da401b27d734188710d2c5c97e3e12900775ee
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-12 17:57:54 +0100

    update switcher

--- src/features/Lang/LanguageSwitcher.tsx
+++ src/features/Lang/LanguageSwitcher.tsx
@@ -5,20 +5,26 @@ import { useLingui } from "@lingui/react";
 import { usePathname, useRouter } from "next/navigation";
 import { FormControl, InputLabel, MenuItem, Select, Stack } from "@mui/material";
 import { fullEnglishLanguageName, SupportedLanguage, supportedLanguages } from "@/common/lang";
+import { getUrlStart } from "./getUrlStart";
 
+const parseLangFromUrl = (pathname: string) => {
+  const potentialLang = pathname?.split("/")[1].trim();
+  const supportedLang = supportedLanguages.find((l) => l === potentialLang) || "en";
+  return supportedLang;
+};
 export function LanguageSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
   const { i18n } = useLingui();
 
-  const [locale, setLocale] = useState<SupportedLanguage>(
-    pathname?.split("/")[1] as SupportedLanguage
-  );
+  const [locale, setLocale] = useState<SupportedLanguage>(parseLangFromUrl(pathname));
   const supportedLang = supportedLanguages.find((l) => l === locale) || "en";
 
   function handleChange(newLang: SupportedLanguage) {
-    const pathNameWithoutLocale = pathname?.split("/")?.slice(2) ?? [];
-    const newPath = `/${newLang}/${pathNameWithoutLocale.join("/")}`;
+    const sliceNumber = locale !== "en" ? 2 : pathname.startsWith("/en") ? 2 : 1;
+
+    const pathNameWithoutLocale = pathname?.split("/")?.slice(sliceNumber) ?? [];
+    const newPath = `${getUrlStart(newLang)}${pathNameWithoutLocale.join("/")}`;
     setLocale(locale);
     router.push(newPath);
   }
