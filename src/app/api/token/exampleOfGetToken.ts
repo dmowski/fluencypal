348e1f9f9ffc1b977821f12ee6b66d07e539ed0e
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-13 22:07:03 +0100

    ref

--- src/app/api/token/exampleOfGetToken.ts
+++ src/app/api/token/exampleOfGetToken.ts
@@ -1,7 +1,7 @@
 import { GetEphemeralTokenRequest, GetEphemeralTokenResponse } from "@/common/requests";
 import { getEphemeralToken } from "./getEphemeralToken";
 
-export async function POST(request: Request) {
+export async function exampleOfPost(request: Request) {
   const aiRequest = (await request.json()) as GetEphemeralTokenRequest;
 
   const model = aiRequest.model;
