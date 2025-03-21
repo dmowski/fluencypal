d4053bafd97de633144ea2f42add66dbab336e07
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-21 19:46:02 +0100

    ref

--- src/app/api/payment/config.ts
+++ src/app/api/payment/config.ts
@@ -1,6 +1,7 @@
 const isStripeLive = process.env.STRIPE_IS_LIVE === "live";
 
 export const stripeConfig = {
+  isStripeLive,
   STRIPE_WEBHOOK_SECRET: isStripeLive
     ? process.env.STRIPE_WEBHOOK_SECRET
     : process.env.STRIPE_WEBHOOK_SECRET_TEST,
