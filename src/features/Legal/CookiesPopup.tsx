3b298350be36910ac3185881b465cc5801adb2f4
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-03-24 16:28:53 +0100

    bar

--- src/features/Legal/CookiesPopup.tsx
+++ src/features/Legal/CookiesPopup.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Link, Stack, Typography } from "@mui/material";
 import { setCookiesGDPR } from "../Firebase/init";
 import { useLocalStorage } from "react-use";
 import { useEffect, useState } from "react";
@@ -9,10 +9,10 @@ import { getUrlStart } from "../Lang/getUrlStart";
 import { usePathname } from "next/navigation";
 
 export const CookiesPopup = () => {
-  const message = `We use cookies to personalize content, analyze our traffic, and provide you with a better user experience. You can accept or reject non-essential cookies.`;
+  const message = `We use cookies to personalize content, analyze our traffic, and provide you with a good user experience. You can accept/reject non-essential cookies.`;
   const ok = "Ok";
   const no = "No";
-  const privacy = "Cookies Policy";
+  const privacy = "Policy";
 
   const [isClosedStore, setClosedStore] = useLocalStorage("cookiesPopup_v1", false);
   const [isClosed, setClosed] = useState(true);
@@ -47,42 +47,62 @@ export const CookiesPopup = () => {
         bottom: "0",
         left: "0",
         right: "0",
-        padding: "5px 0px",
+        padding: "1px 0px 0px 0px",
         width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "rgba(0, 0, 0, 0.9)",
-        borderTop: "1px solid rgba(255, 255, 255, 0.4)",
+        borderTop: "1px solid rgba(0, 0, 0, 0.4)",
         zIndex: 9999999,
         boxSizing: "border-box",
       }}
     >
       <Stack
         sx={{
-          maxWidth: "1400px",
+          maxWidth: "1600px",
           boxSizing: "border-box",
           flexDirection: "row",
           columnGap: "20px",
-
           alignItems: "center",
-          padding: "2px 10px",
-          flexWrap: "wrap",
+          padding: "0px 3px",
         }}
       >
         <Typography
           variant="caption"
-          sx={{ color: "#fff", maxWidth: "800px", lineHeight: "1.2", opacity: 0.7 }}
+          sx={{
+            color: "#fff",
+            maxWidth: "890px",
+            lineHeight: "1.1",
+            opacity: 0.8,
+            fontSize: "0.7rem",
+            "@media (max-width: 600px)": {
+              width: "calc(100%)",
+            },
+          }}
         >
-          {message}
+          {message}{" "}
+          <Link
+            href={`${getUrlStart(lang)}cookies`}
+            target="_blank"
+            rel="noreferrer"
+            sx={{
+              padding: "0px",
+            }}
+          >
+            {privacy}
+          </Link>
         </Typography>
         <Stack
           sx={{
             flexDirection: "row",
-            gap: "10px",
+            gap: "2px",
             alignItems: "center",
             position: "relative",
             zIndex: 9999999,
+            "@media (max-width: 600px)": {
+              flexDirection: "column",
+            },
           }}
         >
           <Button
@@ -94,11 +114,15 @@ export const CookiesPopup = () => {
           >
             {ok}
           </Button>
-          <Button size="small" onClick={onReject}>
+          <Button
+            size="small"
+            onClick={onReject}
+            sx={{
+              padding: "0px",
+              fontSize: "0.7rem",
+            }}
+          >
             {no}
-          </Button>
-          <Button href={`${getUrlStart(lang)}cookies`} size="small">
-            {privacy}
           </Button>
         </Stack>
       </Stack>
