fd9c253ebef1136af994172df0a6d5a9589df989
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-04-07 23:39:47 +0200

    TMP

--- src/features/Auth/SignInForm.tsx
+++ src/features/Auth/SignInForm.tsx
@@ -15,6 +15,8 @@ export const isInAppBrowser = (): boolean => {
   const ua = navigator.userAgent.toLowerCase();
   return (
     ua.includes("instagram") ||
+    ua.includes("tginternal") || // Telegram Android in-app
+    ua.includes("tgapp") || // Possible Telegram identifier
     ua.includes("telegram") ||
     ua.includes("fbav") || // Facebook app
     ua.includes("fb_iab") // Facebook in-app browser
