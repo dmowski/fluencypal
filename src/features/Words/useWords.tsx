1ab255407004079dcc60d04d38a89d4d73fad878
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-04-08 21:34:23 +0200

    words

--- src/features/Words/useWords.tsx
+++ src/features/Words/useWords.tsx
@@ -14,7 +14,7 @@ interface WordsContextType {
   loading: boolean;
   addWordsStatFromText: (text: string) => Promise<string[]>;
   totalWordsCount: number;
-  getNewWordsToLearn: () => Promise<string[]>;
+  getNewWordsToLearn: (topic?: string) => Promise<string[]>;
   removeWordsToLearn: () => void;
   wordsToLearn: string[];
   isGeneratingWords: boolean;
@@ -73,16 +73,18 @@ function useProvideWords(): WordsContextType {
     return await addWords(stat);
   };
 
-  const getNewWordsToLearn = async () => {
+  const getNewWordsToLearn = async (topic?: string) => {
     const dictionary = wordsStats?.dictionary || {};
     const knownWords = Object.keys(dictionary).filter((word) => dictionary[word] > 0);
     setWordsToLearn([]);
     try {
       setIsGeneratingWords(true);
+
       const systemInstruction = [
         `User provides list of works that they knows.
 You should generate list of 10 new words to learn.
 Words should be useful for daily basis usage and not too difficult.
+${topic ? `Follow this topic: ${topic}` : ""}
 Return info in JSON format. Example: ["word1", "word2", "word3"].
 Do not wrap answer with any wrapper phrases.
 Your response will be sent to JSON.parse() function.
