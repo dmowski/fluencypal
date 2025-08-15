09a211aedb01d376312dbf94a16d4f96ff5622dc
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-08-15 22:38:53 +0200

    TMP

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.0.223"],
   experimental: {
     swcPlugins: [
       [
