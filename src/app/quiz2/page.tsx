df57d2d877eb685f4860764a846694a338e87c65
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-09-01 10:45:19 +0200

    quiz2

---
+++ src/app/quiz2/page.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { supportedLanguages } from "@/features/Lang/lang";
+import { initLingui } from "@/initLingui";
+import { allMessages } from "@/appRouterI18n";
+import { LinguiClientProvider } from "@/features/Lang/LinguiClientProvider";
+import { generateMetadataInfo } from "@/libs/metadata";
+import { QuizPage } from "@/features/Goal/QuizPage";
+import { PracticeProvider } from "@/app/practiceProvider";
+
+interface PageProps {
+  params: Promise<{ lang: string }>;
+  searchParams: Promise<{
+    learn?: string;
+  }>;
+}
+export async function generateStaticParams() {
+  return [];
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+  const searchParam = await props.searchParams;
+  const toLearn = searchParam.learn || "";
+  const languageToLearn = supportedLanguages.find((l) => l === toLearn) || "en";
+  return generateMetadataInfo({
+    lang: "en",
+    currentPath: "quiz2",
+    languageToLearn,
+  });
+}
+
+export default async function Page(props: PageProps) {
+  const lang = (await props.params).lang;
+
+  const supportedLang = supportedLanguages.find((l) => l === lang) || "en";
+  initLingui(supportedLang);
+
+  const searchParam = await props.searchParams;
+  const toLearn = searchParam.learn || "";
+  const languageToLearn = supportedLanguages.find((l) => l === toLearn) || "en";
+
+  return (
+    <LinguiClientProvider
+      initialLocale={supportedLang}
+      initialMessages={allMessages[supportedLang]!}
+    >
+      <PracticeProvider>
+        <QuizPage lang={supportedLang} defaultLangToLearn={languageToLearn} />
+      </PracticeProvider>
+    </LinguiClientProvider>
+  );
+}
