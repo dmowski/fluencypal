f5d8141c4988d50079cd792e33dea8a14673cea3
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-09-01 10:47:00 +0200

    Replace QuizPage with QuizPage2 in quiz2 page and add QuizPage2 component

--- src/app/quiz2/page.tsx
+++ src/app/quiz2/page.tsx
@@ -4,8 +4,8 @@ import { initLingui } from "@/initLingui";
 import { allMessages } from "@/appRouterI18n";
 import { LinguiClientProvider } from "@/features/Lang/LinguiClientProvider";
 import { generateMetadataInfo } from "@/libs/metadata";
-import { QuizPage } from "@/features/Goal/QuizPage";
 import { PracticeProvider } from "@/app/practiceProvider";
+import { QuizPage2 } from "@/features/Goal/Quiz/QuizPage2";
 
 interface PageProps {
   params: Promise<{ lang: string }>;
@@ -44,7 +44,7 @@ export default async function Page(props: PageProps) {
       initialMessages={allMessages[supportedLang]!}
     >
       <PracticeProvider>
-        <QuizPage lang={supportedLang} defaultLangToLearn={languageToLearn} />
+        <QuizPage2 lang={supportedLang} defaultLangToLearn={languageToLearn} />
       </PracticeProvider>
     </LinguiClientProvider>
   );
