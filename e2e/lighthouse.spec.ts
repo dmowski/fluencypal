0e19242c9dbc06795f1f88216b42de2762f9ae64
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-11-29 23:40:46 +0100

    refactor: update Lighthouse audit tests and configuration settings

--- e2e/lighthouse.spec.ts
+++ e2e/lighthouse.spec.ts
@@ -3,13 +3,40 @@ import { playAudit } from "playwright-lighthouse";
 import type { Page } from "@playwright/test";
 
 test.describe("Lighthouse Audit", () => {
-  test("Home page should have good SEO", async ({ page }: { page: Page }) => {
+  // increase timeout for lighthouse audits
+  test.setTimeout(120000);
+  test("Home page should have good metrics", async ({ page }: { page: Page }) => {
     await page.goto("/");
 
     await playAudit({
       page,
       thresholds: {
         seo: 100,
+        performance: 65,
+        accessibility: 95,
+        "best-practices": 95,
+      },
+      port: 9222,
+      reports: {
+        formats: {
+          html: true,
+        },
+        name: "lighthouse-report",
+        directory: "test-results/lighthouse-reports",
+      },
+    });
+  });
+
+  test("Home page (RU) should have good metrics", async ({ page }: { page: Page }) => {
+    await page.goto("/ru");
+
+    await playAudit({
+      page,
+      thresholds: {
+        seo: 100,
+        performance: 65,
+        accessibility: 95,
+        "best-practices": 95,
       },
       port: 9222,
       reports: {
