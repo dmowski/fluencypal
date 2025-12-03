5d486b0df70097d8f3ca18395ff274d4f387c5fa
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-03 15:32:50 +0100

    menu

--- src/features/Interview/Landing/InterviewHeader.tsx
+++ src/features/Interview/Landing/InterviewHeader.tsx
@@ -4,7 +4,7 @@ import { HeaderLink } from "@/features/Header/types";
 import { getUrlStart } from "@/features/Lang/getUrlStart";
 import { SupportedLanguage } from "@/features/Lang/lang";
 import { useLingui } from "@lingui/react";
-import { Pickaxe } from "lucide-react";
+import { BadgeQuestionMark, Gem, Pickaxe, Send } from "lucide-react";
 
 export interface HeaderProps {
   lang: SupportedLanguage;
@@ -17,15 +17,41 @@ export function InterviewHeader({ lang, interviewId }: HeaderProps) {
   const pageUrl = getUrlStart(lang) + page;
   const { i18n } = useLingui();
 
-  const links: HeaderLink[] = [
-    {
-      title: i18n._("How it works"),
-      icon: Pickaxe,
-      href: page + "#how-it-works",
-    },
-  ];
-
   return (
-    <HeaderUI transparentOnTop={true} lang={lang} links={links} buttons={[]} logoHref={pageUrl} />
+    <HeaderUI
+      transparentOnTop={true}
+      lang={lang}
+      links={[
+        {
+          title: i18n._("How it works"),
+          icon: Pickaxe,
+          href: page + "#how-it-works",
+        },
+        {
+          title: i18n._("Contacts"),
+          icon: Send,
+          href: page + "#contacts",
+        },
+        {
+          title: i18n._("Price"),
+          icon: Gem,
+          href: page + "#price",
+        },
+
+        {
+          title: i18n._("FAQ"),
+          icon: BadgeQuestionMark,
+          href: page + "#faq",
+        },
+      ]}
+      buttons={[
+        {
+          title: i18n._("Start Free Trial"),
+          href: page + "#start-free-trial",
+          isSolid: true,
+        },
+      ]}
+      logoHref={pageUrl}
+    />
   );
 }
