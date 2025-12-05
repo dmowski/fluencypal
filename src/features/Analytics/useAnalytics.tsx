0d9ecf19d574ed9e23f27eccdb3a0e97546ba9e2
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-06 00:47:28 +0100

    Initialize Sentry in useAnalytics for improved error tracking

--- src/features/Analytics/useAnalytics.tsx
+++ src/features/Analytics/useAnalytics.tsx
@@ -6,6 +6,44 @@ import * as Sentry from "@sentry/nextjs";
 
 const RUN_ON_DEV_ENV = true;
 
+const initSentry = () => {
+  Sentry.init({
+    dsn: "https://f683d729da9d8855c7742f03c0caaf55@o4506187426103296.ingest.us.sentry.io/4508885116452864",
+
+    // Add optional integrations for additional features
+    integrations: [],
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0,
+
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 0,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+    enabled: true,
+
+    /*
+  sendDefaultPii: false,
+
+  beforeSend(event) {
+    // Remove IP address
+    if (event.user) {
+      delete event.user.ip_address;
+    }
+
+    // Remove other identifying data
+    event.user = undefined;
+    return event;
+  },*/
+  });
+};
+
 const confirmGtag = async () => {
   if (isDev() && !RUN_ON_DEV_ENV) {
     console.log("Skipping gtag in dev mode");
@@ -87,6 +125,7 @@ export const AnalyticsProvider = ({ children }: { children: ReactNode }) => {
       return;
     }
     isInitialized.current = true;
+    initSentry();
     initHotjar();
     initGTag();
   }, [auth.uid]);
