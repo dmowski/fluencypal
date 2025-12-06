4845667ef4eef39d6a2d8143eb98bc4082264f77
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-06 10:25:21 +0100

    ref

--- src/features/Goal/Quiz/ProgressBar.tsx
+++ src/features/Goal/Quiz/ProgressBar.tsx
@@ -1,36 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
-import { useWindowSizes } from "@/features/Layout/useWindowSizes";
-import { useTgNavigation } from "@/features/Telegram/useTgNavigation";
 import { useQuiz } from "./useQuiz";
-import { UIProgressBar } from "../../Survey/UIProgressBar";
+import { QuizProgressBar } from "./components/QuizProgressBar";
 
 export const ProgressBar = () => {
-  const { topOffset } = useWindowSizes();
   const { navigateToMainPage, isCanGoToMainPage, isFirstStep, prevStep, progress } = useQuiz();
-  const tgNavigation = useTgNavigation();
-
-  useEffect(() => {
-    const off = tgNavigation.addBackHandler(prevStep);
-    return off;
-  }, [prevStep]);
-
-  const handleBackClick = () => {
-    if (isFirstStep) {
-      isCanGoToMainPage && navigateToMainPage();
-    } else {
-      prevStep();
-    }
-  };
 
   return (
-    <UIProgressBar
-      topOffset={topOffset}
+    <QuizProgressBar
+      navigateToMainPage={navigateToMainPage}
       isCanGoToMainPage={isCanGoToMainPage}
       isFirstStep={isFirstStep}
+      prevStep={prevStep}
       progress={progress}
-      onBackClick={handleBackClick}
     />
   );
 };
---
+++ src/features/Goal/Quiz/components/QuizProgressBar.tsx
@@ -0,0 +1,44 @@
+import { useWindowSizes } from "@/features/Layout/useWindowSizes";
+import { UIProgressBar } from "@/features/Survey/UIProgressBar";
+import { useTgNavigation } from "@/features/Telegram/useTgNavigation";
+import { useEffect } from "react";
+
+export const QuizProgressBar = ({
+  navigateToMainPage,
+  isCanGoToMainPage,
+  isFirstStep,
+  prevStep,
+  progress,
+}: {
+  navigateToMainPage: () => void;
+  isCanGoToMainPage: boolean;
+  isFirstStep: boolean;
+  prevStep: () => void;
+  progress: number;
+}) => {
+  const { topOffset } = useWindowSizes();
+  const tgNavigation = useTgNavigation();
+
+  useEffect(() => {
+    const off = tgNavigation.addBackHandler(prevStep);
+    return off;
+  }, [prevStep]);
+
+  const handleBackClick = () => {
+    if (isFirstStep) {
+      isCanGoToMainPage && navigateToMainPage();
+    } else {
+      prevStep();
+    }
+  };
+
+  return (
+    <UIProgressBar
+      topOffset={topOffset}
+      isCanGoToMainPage={isCanGoToMainPage}
+      isFirstStep={isFirstStep}
+      progress={progress}
+      onBackClick={handleBackClick}
+    />
+  );
+};
