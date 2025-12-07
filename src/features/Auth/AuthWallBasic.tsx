29ba811915b5db0ac40d6f5a9e64ab8bd546d45f
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-07 21:30:48 +0100

    ref

---
+++ src/features/Auth/AuthWallBasic.tsx
@@ -0,0 +1,256 @@
+import { ReactNode, useEffect, useState } from "react";
+import Google from "@mui/icons-material/Google";
+import { Stack, Typography } from "@mui/material";
+import { useLingui } from "@lingui/react";
+import { Trans } from "@lingui/react/macro";
+import { Check, Cookie, PencilRuler, ScrollText, ShieldCheck } from "lucide-react";
+import { scrollTopFast } from "@/libs/scroll";
+import { InfoStep } from "../Survey/InfoStep";
+import { IconTextList, ListItem } from "../Survey/IconTextList";
+import { getUrlStart } from "../Lang/getUrlStart";
+import { useAuth } from "./useAuth";
+
+interface AuthWallBasicProps {
+  children: ReactNode;
+  featuresTitle: ReactNode;
+  featuresSubTitle: ReactNode;
+  featuresList: ListItem[];
+  authTitle: ReactNode;
+  authSubTitle: ReactNode;
+  authList: ListItem[];
+}
+
+export const AuthWallBasic = ({
+  children,
+  featuresTitle,
+  featuresSubTitle,
+  featuresList,
+  authTitle,
+  authSubTitle,
+  authList,
+}: AuthWallBasicProps) => {
+  const auth = useAuth();
+  const { i18n } = useLingui();
+
+  const isShowAuthWall = !auth.uid && !auth.loading;
+
+  const steps = ["features", "agreement", "auth"] as const;
+  const [step, setStep] = useState<(typeof steps)[number]>(steps[0]);
+
+  const nextStep = () => {
+    const currentIndex = steps.indexOf(step);
+    if (currentIndex < steps.length - 1) {
+      setStep(steps[currentIndex + 1]);
+    }
+
+    if (step === "auth") {
+      auth.signInWithGoogle();
+    }
+  };
+
+  useEffect(() => {
+    if (isShowAuthWall) {
+      const isWindow = typeof window !== "undefined";
+      if (isWindow) {
+        scrollTopFast();
+      }
+    }
+  }, [isShowAuthWall]);
+
+  if (!isShowAuthWall) {
+    return children;
+  }
+
+  return (
+    <Stack
+      component={"main"}
+      sx={{
+        width: "100%",
+        paddingTop: `10px`,
+        paddingBottom: `10px`,
+        alignItems: "center",
+      }}
+    >
+      <Stack
+        sx={{
+          maxWidth: "600px",
+          padding: "0 10px",
+          width: "100%",
+          "--content-max-width": "380px",
+          "--content-min-height": "350px",
+        }}
+      >
+        {step === "features" && (
+          <InfoStep
+            imageUrl="/avatar/bot2.webp"
+            actionButtonTitle={i18n._("Next")}
+            subComponent={
+              <Stack
+                sx={{
+                  gap: "20px",
+                  alignItems: "flex-start",
+                  maxWidth: "var(--content-max-width)",
+                  width: "100%",
+                  minHeight: "var(--content-min-height)",
+                }}
+              >
+                <Stack>
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      fontWeight: 660,
+                    }}
+                  >
+                    {featuresTitle}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      opacity: 0.7,
+                    }}
+                  >
+                    {featuresSubTitle}
+                  </Typography>
+                </Stack>
+                <Stack
+                  sx={{
+                    width: "max-content",
+                    minWidth: "230px",
+                    maxWidth: "100%",
+                  }}
+                >
+                  <IconTextList listItems={featuresList} />
+                </Stack>
+              </Stack>
+            }
+            onClick={nextStep}
+          />
+        )}
+
+        {step === "agreement" && (
+          <InfoStep
+            actionButtonTitle={i18n._("I agree")}
+            actionButtonEndIcon={<Check />}
+            imageUrl="/avatar/bot1.png"
+            subComponent={
+              <Stack
+                sx={{
+                  gap: "20px",
+                  alignItems: "flex-start",
+                  maxWidth: "var(--content-max-width)",
+                  width: "100%",
+                  minHeight: "var(--content-min-height)",
+                }}
+              >
+                <Stack>
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      fontWeight: 660,
+                    }}
+                  >
+                    <Trans>We will speak freely</Trans>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      opacity: 0.7,
+                    }}
+                  >
+                    <Trans>So we need your agreement with that</Trans>
+                  </Typography>
+                </Stack>
+                <Stack
+                  sx={{
+                    minWidth: "230px",
+                    width: "100%",
+                    gap: "10px",
+                  }}
+                >
+                  <IconTextList
+                    listItems={[
+                      {
+                        title: i18n._("We process your voice using AI"),
+                        icon: ShieldCheck,
+                      },
+                      {
+                        title: i18n._("Your transcripts are securely stored in our service"),
+                        icon: ShieldCheck,
+                      },
+                      {
+                        title: i18n._("You can delete your personal data anytime"),
+                        icon: ShieldCheck,
+                      },
+                      {
+                        title: i18n._("We use cookies to enhance your experience"),
+                        icon: Cookie,
+                      },
+                      {
+                        title: i18n._("Privacy Policy"),
+                        icon: ScrollText,
+                        href: `${getUrlStart("en")}privacy`,
+                      },
+                      {
+                        title: i18n._("Terms of Use"),
+                        icon: PencilRuler,
+                        href: `${getUrlStart("en")}terms`,
+                      },
+                    ]}
+                  />
+                </Stack>
+              </Stack>
+            }
+            onClick={nextStep}
+          />
+        )}
+
+        {step === "auth" && (
+          <InfoStep
+            imageUrl="/avatar/map.webp"
+            actionButtonTitle={i18n._("Sign in with Google")}
+            actionButtonStartIcon={<Google />}
+            subComponent={
+              <Stack
+                sx={{
+                  gap: "20px",
+                  alignItems: "flex-start",
+                  maxWidth: "var(--content-max-width)",
+                  width: "100%",
+                  minHeight: "var(--content-min-height)",
+                }}
+              >
+                <Stack>
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      fontWeight: 660,
+                    }}
+                  >
+                    {authTitle}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      opacity: 0.7,
+                    }}
+                  >
+                    {authSubTitle}
+                  </Typography>
+                </Stack>
+                <Stack
+                  sx={{
+                    width: "100%",
+                    minWidth: "230px",
+                  }}
+                >
+                  <IconTextList listItems={authList} />
+                </Stack>
+              </Stack>
+            }
+            onClick={nextStep}
+          />
+        )}
+      </Stack>
+    </Stack>
+  );
+};
--- src/features/Auth/InterviewAuthWall.tsx
+++ src/features/Auth/InterviewAuthWall.tsx
@@ -1,304 +1,72 @@
-import { ReactNode, useEffect, useState } from "react";
-import { useAuth } from "./useAuth";
+import { ReactNode } from "react";
 import { useLingui } from "@lingui/react";
-import { scrollTopFast } from "@/libs/scroll";
-import { Stack, Typography } from "@mui/material";
 import { Trans } from "@lingui/react/macro";
 import {
   BetweenHorizontalStart,
   Bird,
-  BookType,
-  Check,
-  Cookie,
   GraduationCap,
   Lightbulb,
   PencilRuler,
   ScrollText,
-  ShieldCheck,
   Sparkles,
   Speech,
   UsersRound,
 } from "lucide-react";
+import { AuthWallBasic } from "./AuthWallBasic";
 import { getUrlStart } from "../Lang/getUrlStart";
-import Google from "@mui/icons-material/Google";
-import { InfoStep } from "../Survey/InfoStep";
-import { IconTextList } from "../Survey/IconTextList";
 
 export const InterviewAuthWall = ({ children }: { children: ReactNode }) => {
-  const auth = useAuth();
   const { i18n } = useLingui();
-
-  const isShowAuthWall = !auth.uid && !auth.loading;
-
-  const steps = ["features", "agreement", "auth"];
-  const [step, setStep] = useState(steps[0]);
-  const nextStep = () => {
-    const currentIndex = steps.indexOf(step);
-    if (currentIndex < steps.length - 1) {
-      setStep(steps[currentIndex + 1]);
-    }
-
-    if (step === "auth") {
-      auth.signInWithGoogle();
-    }
-  };
-
-  useEffect(() => {
-    if (isShowAuthWall) {
-      const isWindow = typeof window !== "undefined";
-      if (isWindow) {
-        scrollTopFast();
-      }
-    }
-  }, [isShowAuthWall]);
-
-  if (!isShowAuthWall) {
-    return children;
-  }
-
   return (
-    <Stack
-      component={"main"}
-      sx={{
-        width: "100%",
-        paddingTop: `10px`,
-        paddingBottom: `10px`,
-        alignItems: "center",
-      }}
+    <AuthWallBasic
+      featuresTitle={<Trans>What you get with FluencyPal</Trans>}
+      featuresSubTitle={<Trans>Your job interview preparation partner</Trans>}
+      featuresList={[
+        {
+          title: i18n._("Daily interview without fear of judgment"),
+          icon: Speech,
+        },
+        {
+          title: i18n._("Corrections when you get stuck"),
+          icon: Sparkles,
+        },
+        {
+          title: i18n._("Personalized plan tailored to your goals"),
+          icon: Lightbulb,
+        },
+        {
+          title: i18n._("Hints to boost your confidence"),
+          icon: GraduationCap,
+        },
+        {
+          title: i18n._("Practice STAR method answers"),
+          icon: UsersRound,
+        },
+      ]}
+      authTitle={<Trans>Let's create an account</Trans>}
+      authSubTitle={<Trans>So you can keep your progress</Trans>}
+      authList={[
+        {
+          title: i18n._("Credit card is required for verification only"),
+          icon: Bird,
+        },
+        {
+          title: i18n._("No ads, no spam"),
+          icon: BetweenHorizontalStart,
+        },
+        {
+          title: i18n._("Privacy Policy"),
+          icon: ScrollText,
+          href: `${getUrlStart("en")}privacy`,
+        },
+        {
+          title: i18n._("Terms of Use"),
+          icon: PencilRuler,
+          href: `${getUrlStart("en")}terms`,
+        },
+      ]}
     >
-      <Stack
-        sx={{
-          maxWidth: "600px",
-          padding: "0 10px",
-          width: "100%",
-          "--content-max-width": "380px",
-          "--content-min-height": "350px",
-        }}
-      >
-        {step === "features" && (
-          <InfoStep
-            imageUrl="/avatar/bot2.webp"
-            actionButtonTitle={i18n._("Next")}
-            subComponent={
-              <Stack
-                sx={{
-                  gap: "20px",
-                  alignItems: "flex-start",
-                  maxWidth: "var(--content-max-width)",
-                  width: "100%",
-                  minHeight: "var(--content-min-height)",
-                }}
-              >
-                <Stack sx={{}}>
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      fontWeight: 660,
-                    }}
-                  >
-                    {<Trans>What you get with FluencyPal</Trans>}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      opacity: 0.7,
-                    }}
-                  >
-                    {<Trans>Your job interview preparation partner</Trans>}
-                  </Typography>
-                </Stack>
-                <Stack
-                  sx={{
-                    width: "max-content",
-                    minWidth: "230px",
-                    maxWidth: "100%",
-                  }}
-                >
-                  <IconTextList
-                    listItems={[
-                      {
-                        title: i18n._("Daily interview without fear of judgment"),
-                        icon: Speech,
-                      },
-                      {
-                        title: i18n._("Corrections when you get stuck"),
-                        icon: Sparkles,
-                      },
-
-                      {
-                        title: i18n._("Personalized plan tailored to your goals"),
-                        icon: Lightbulb,
-                      },
-
-                      {
-                        title: i18n._("Hints to boost your confidence"),
-                        icon: GraduationCap,
-                      },
-                      {
-                        title: i18n._("Practice STAR method answers"),
-                        icon: UsersRound,
-                      },
-                    ]}
-                  />
-                </Stack>
-              </Stack>
-            }
-            onClick={nextStep}
-          />
-        )}
-        {step === "agreement" && (
-          <InfoStep
-            actionButtonTitle={i18n._("I agree")}
-            actionButtonEndIcon={<Check />}
-            imageUrl="/avatar/bot1.png"
-            subComponent={
-              <Stack
-                sx={{
-                  gap: "20px",
-                  alignItems: "flex-start",
-                  maxWidth: "var(--content-max-width)",
-                  width: "100%",
-                  minHeight: "var(--content-min-height)",
-                }}
-              >
-                <Stack sx={{}}>
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      fontWeight: 660,
-                    }}
-                  >
-                    <Trans>We will speak freely</Trans>
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      opacity: 0.7,
-                    }}
-                  >
-                    <Trans>So we need your agreement with that</Trans>
-                  </Typography>
-                </Stack>
-                <Stack
-                  sx={{
-                    minWidth: "230px",
-                    width: "100%",
-                    gap: "10px",
-                  }}
-                >
-                  <IconTextList
-                    listItems={[
-                      {
-                        title: i18n._("We process your voice using AI"),
-                        icon: ShieldCheck,
-                      },
-
-                      {
-                        title: i18n._("Your transcripts are securely stored in our service"),
-                        icon: ShieldCheck,
-                      },
-
-                      {
-                        title: i18n._("You can delete your personal data anytime"),
-                        icon: ShieldCheck,
-                      },
-
-                      {
-                        title: i18n._("We use cookies to enhance your experience"),
-                        icon: Cookie,
-                      },
-
-                      {
-                        title: i18n._("Privacy Policy"),
-                        icon: ScrollText,
-                        href: `${getUrlStart("en")}privacy`,
-                      },
-
-                      {
-                        title: i18n._("Terms of Use"),
-                        icon: PencilRuler,
-                        href: `${getUrlStart("en")}terms`,
-                      },
-                    ]}
-                  />
-                </Stack>
-              </Stack>
-            }
-            onClick={nextStep}
-          />
-        )}
-
-        {step === "auth" && (
-          <InfoStep
-            imageUrl="/avatar/map.webp"
-            actionButtonTitle={i18n._("Sign in with Google")}
-            actionButtonStartIcon={<Google />}
-            subComponent={
-              <Stack
-                sx={{
-                  gap: "20px",
-                  alignItems: "flex-start",
-                  maxWidth: "var(--content-max-width)",
-                  width: "100%",
-                  minHeight: "var(--content-min-height)",
-                }}
-              >
-                <Stack>
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      fontWeight: 660,
-                    }}
-                  >
-                    <Trans>Let's create an account</Trans>
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      opacity: 0.7,
-                    }}
-                  >
-                    <Trans>So you can keep your progress</Trans>
-                  </Typography>
-                </Stack>
-                <Stack
-                  sx={{
-                    width: "100%",
-                    minWidth: "230px",
-                  }}
-                >
-                  <IconTextList
-                    listItems={[
-                      {
-                        title: i18n._("Credit card is required for verification only"),
-                        icon: Bird,
-                      },
-
-                      {
-                        title: i18n._("No ads, no spam"),
-                        icon: BetweenHorizontalStart,
-                      },
-
-                      {
-                        title: i18n._("Privacy Policy"),
-                        icon: ScrollText,
-                        href: `${getUrlStart("en")}privacy`,
-                      },
-
-                      {
-                        title: i18n._("Terms of Use"),
-                        icon: PencilRuler,
-                        href: `${getUrlStart("en")}terms`,
-                      },
-                    ]}
-                  />
-                </Stack>
-              </Stack>
-            }
-            onClick={nextStep}
-          />
-        )}
-      </Stack>
-    </Stack>
+      {children}
+    </AuthWallBasic>
   );
 };
