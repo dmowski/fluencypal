d25c35df6c418b60487c58f53b4e54e7e5aca657
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-18 22:08:42 +0100

    ref

--- src/app/api/telegram/sendTelegramMessage.ts
+++ src/app/api/telegram/sendTelegramMessage.ts
@@ -5,19 +5,14 @@ const TELEGRAM_API_KEY = process.env.TELEGRAM_API_KEY || "";
 const TELEGRAM_SUPPORT_CHAT_ID = process.env.TELEGRAM_SUPPORT_CHAT_ID || "";
 
 const sendTelegramMessage = async (message: string, chatId: string): Promise<void> => {
-  const formattedMessage = encodeURIComponent(message.replace(/"/g, '\\"'));
-
-  const urlForSend = [
-    `https://api.telegram.org/bot${TELEGRAM_API_KEY}/`,
-    `sendMessage?chat_id=${chatId}&`,
-    `text=${formattedMessage}&`,
-    `parse_mode=Markdown`,
-  ].join("");
+  const url = new URL(`https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage`);
+  url.searchParams.set("chat_id", chatId);
+  url.searchParams.set("text", message);
+  url.searchParams.set("parse_mode", "Markdown");
 
   try {
-    console.log("urlForSend", urlForSend);
-    const result = await fetch(urlForSend);
-
+    console.log("urlForSend", url);
+    const result = await fetch(url);
     const resultJson = await result.json();
     const isDebug = false;
     if (isDebug) {
