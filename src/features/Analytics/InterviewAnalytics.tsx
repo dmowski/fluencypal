3051b94305c78c8e7645185097b53509530e40f6
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-18 12:17:49 +0100

    init gtag

--- src/features/Analytics/InterviewAnalytics.tsx
+++ src/features/Analytics/InterviewAnalytics.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef } from "react";
 import { initHotjar } from "./initHotjar";
 import { isDev } from "./isDev";
+import { initGTag } from "./initGTag";
 
 export const InterviewAnalytics = () => {
   const isInitialized = useRef(false);
@@ -14,6 +15,7 @@ export const InterviewAnalytics = () => {
 
     console.log("Init hotjar from client");
     initHotjar();
+    initGTag();
   }, []);
   return <></>;
 };
