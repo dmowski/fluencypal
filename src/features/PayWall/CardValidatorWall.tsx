c79ed3477b73d0a7bb036be082bfc71d3f24199e
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-19 19:11:18 +0100

    ref

--- src/features/PayWall/CardValidatorWall.tsx
+++ src/features/PayWall/CardValidatorWall.tsx
@@ -3,7 +3,7 @@
 import { Button, CircularProgress, Stack, Typography } from "@mui/material";
 import { useSettings } from "../Settings/useSettings";
 import { useLingui } from "@lingui/react";
-import { CreditCard } from "lucide-react";
+import { CreditCard, Gem } from "lucide-react";
 import { useUsage } from "../Usage/useUsage";
 import { useEffect, useState } from "react";
 import { useAuth } from "../Auth/useAuth";
@@ -14,7 +14,8 @@ import { SupportedLanguage } from "../Lang/lang";
 import { CustomModal } from "../uiKit/Modal/CustomModal";
 import { createSetupIntentRequest } from "./createSetupIntentRequest";
 import { VerifyCard } from "./CardValidator";
-import { useAiConversation } from "../Conversation/useAiConversation";
+import { useChatHistory } from "../ConversationHistory/useChatHistory";
+import { ColorIconTextList } from "../Survey/ColorIconTextList";
 
 export const CardValidatorWall = ({ lang }: { lang: SupportedLanguage }) => {
   const { i18n } = useLingui();
@@ -27,8 +28,10 @@ export const CardValidatorWall = ({ lang }: { lang: SupportedLanguage }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [clientSecret, setClientSecret] = useState<string | null>(null);
   const [isTg, setIsTg] = useState(false);
-  const conversation = useAiConversation();
-  const activeConversationMessageCount = conversation.conversation.length;
+  const conversation = useChatHistory();
+  const activeConversationMessageCount = conversation.conversations.length;
+
+  const [isShowHowToUseForFree, setIsShowHowToUseForFree] = useState(false);
 
   useEffect(() => {
     const isTelegramApp = isTMA();
@@ -53,6 +56,7 @@ export const CardValidatorWall = ({ lang }: { lang: SupportedLanguage }) => {
   if (isCreditCardConfirmed || isLoadingSettings || isTg) return <></>;
   if (usage.isFullAccess && !isNewUser) return <></>;
   if (activeConversationMessageCount < 3) return <></>;
+
   return (
     <CustomModal isOpen={true}>
       <Stack
@@ -79,29 +83,64 @@ export const CardValidatorWall = ({ lang }: { lang: SupportedLanguage }) => {
                   gap: "25px",
                 }}
               >
+                {isShowHowToUseForFree ? (
+                  <Stack>
+                    <Typography variant="h3">{i18n._("How to use for free")}</Typography>
+                    <Typography variant="body2">
+                      {i18n._("Simple steps to get the app for free.")}
+                    </Typography>
+                  </Stack>
+                ) : (
+                  <Stack>
+                    <Typography variant="h3">{i18n._("Credit Card Check")}</Typography>
+                    <Typography variant="body2">
+                      {i18n._("Please confirm your credit card to access all features.")}
+                    </Typography>
+                  </Stack>
+                )}
                 <Stack>
-                  <Typography variant="h3">{i18n._("Credit Card Check")}</Typography>
-                  <Typography variant="body2">
-                    {i18n._("Please confirm your credit card to access all features.")}
-                  </Typography>
-                </Stack>
-                <Stack>
-                  <IconTextList
+                  <ColorIconTextList
                     gap="10px"
-                    listItems={[
-                      {
-                        iconName: "sparkles",
-                        title: i18n._("You will get 3 days of full access for free"),
-                      },
-                      {
-                        iconName: "piggy-bank",
-                        title: i18n._("No automatic payment after the trial"),
-                      },
-                      {
-                        iconName: "shield-check",
-                        title: i18n._("No charge will be made"),
-                      },
-                    ]}
+                    listItems={
+                      isShowHowToUseForFree
+                        ? [
+                            {
+                              iconName: "credit-card",
+                              title: i18n._("1. Confirm your credit card details"),
+                            },
+                            {
+                              iconName: "piggy-bank",
+                              title: i18n._("2. Complete the trial period"),
+                            },
+                            {
+                              iconName: "x",
+                              title: i18n._("3. Don't pay anything"),
+                            },
+                            {
+                              iconName: "swords",
+                              title: i18n._("4. Play the game and become one of the top 5 players"),
+                            },
+                            {
+                              iconName: "trophy",
+                              title: i18n._("5. Winners get full access for free "),
+                            },
+                          ]
+                        : [
+                            {
+                              iconName: "sparkles",
+                              title: i18n._("You will get 3 days of full access for free"),
+                            },
+                            {
+                              iconName: "piggy-bank",
+                              title: i18n._("No automatic payment after the trial"),
+                            },
+                            {
+                              iconName: "shield-check",
+                              title: i18n._("No charge will be made"),
+                            },
+                          ]
+                    }
+                    iconSize={"24px"}
                   />
                 </Stack>
               </Stack>
@@ -115,13 +154,23 @@ export const CardValidatorWall = ({ lang }: { lang: SupportedLanguage }) => {
                 }}
               >
                 <Button
-                  variant="contained"
+                  variant={"contained"}
                   size="large"
                   color="info"
                   startIcon={<CreditCard />}
                   onClick={() => onStartValidation()}
                 >
                   {i18n._("Confirm Credit Card")}
+                </Button>
+
+                <Button
+                  variant={isShowHowToUseForFree ? "outlined" : "text"}
+                  size="large"
+                  color="info"
+                  endIcon={<Gem />}
+                  onClick={() => setIsShowHowToUseForFree(!isShowHowToUseForFree)}
+                >
+                  {i18n._("How to use for free")}
                 </Button>
               </Stack>
             </>
