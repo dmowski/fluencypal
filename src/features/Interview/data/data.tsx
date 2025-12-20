68162459e2a686d97f48b3b0d94f66e79ec66587
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-21 00:14:08 +0100

    REMOVE FRONT SIMPLE

--- src/features/Interview/data/data.tsx
+++ src/features/Interview/data/data.tsx
@@ -2,8 +2,6 @@ import { SupportedLanguage } from "@/features/Lang/lang";
 import { getI18nInstance } from "@/appRouterI18n";
 import { InterviewCategory, InterviewData } from "../types";
 import getFrontendLandingData from "./frontend-senior/landing";
-import getFrontendArtData from "./front-art/landing";
-import getSimpleFrontendLandingData from "./frontend-senior-simple/landing";
 import getCsharpBackendDeveloperData from "./backend/landing";
 
 export interface InterviewAllData {
@@ -17,8 +15,6 @@ export const getAllInterviews = (lang: SupportedLanguage): InterviewAllData => {
   const list: InterviewData[] = [
     getFrontendLandingData(lang),
     getCsharpBackendDeveloperData(lang),
-    getSimpleFrontendLandingData(lang),
-    getFrontendArtData(lang),
 
     /*{
       id: "junior-frontend-developer",
