8e35788619004503da5bb42e9ca9273b47de8415
Alexandr Dmowski <dmowski.alex@gmail.com>
2025-12-21 22:00:07 +0100

    ref

---
+++ src/app/api/sitemap/generateSitemap.ts
@@ -0,0 +1,155 @@
+import { SupportedLanguage, supportedLanguages } from "@/features/Lang/lang";
+import { getBlogs } from "@/features/Blog/blogData";
+import { getRolePlayScenarios } from "@/features/RolePlay/rolePlayData";
+
+const updateTime = "2025-08-18T10:01:02+00:00";
+
+interface UrlDefinition {
+  path: string;
+  priority:
+    | "1.0000"
+    | "0.9000"
+    | "0.8000"
+    | "0.7000"
+    | "0.6000"
+    | "0.5000"
+    | "0.4000"
+    | "0.3000"
+    | "0.2000"
+    | "0.1000";
+}
+
+const baseUrl = "https://www.fluencypal.com";
+
+const generateUrl = (url: UrlDefinition) => {
+  const isLangLanding = supportedLanguages.includes(url.path as unknown as SupportedLanguage);
+
+  const path = url.path === "" ? "/" : `/${url.path}`;
+
+  const fullUrl = `${baseUrl}${path}`;
+  const defaultLangHref = isLangLanding ? baseUrl + "/" : `${fullUrl}`;
+
+  return `<url>
+    <loc>${fullUrl}</loc>
+    <lastmod>${updateTime}</lastmod>
+    <priority>${url.priority}</priority>
+    <xhtml:link rel="alternate" hreflang="en" href="${defaultLangHref}"/>
+${supportedLanguages
+  .filter((lang) => lang !== "en")
+  .map((lang) => {
+    const pathWithLang = isLangLanding ? `${lang}` : `${lang}${url.path === "" ? "" : path}`;
+    return `        <xhtml:link rel="alternate" hreflang="${lang}" href="${baseUrl}/${pathWithLang}"/>`;
+  })
+  .join("\n")}
+    <xhtml:link rel="alternate" hreflang="x-default" href="${defaultLangHref}"/>
+</url>`;
+};
+
+export async function generateSitemap(): Promise<string> {
+  const localeLinks: UrlDefinition[] = supportedLanguages
+    .filter((lang) => lang !== "en")
+    .map((lang) => ({
+      path: lang,
+      priority: "0.7000",
+    }));
+
+  const scenariosData = getRolePlayScenarios("en");
+  const rolePlayScenarios = scenariosData.rolePlayScenarios;
+  const rolePlayCategories = scenariosData.categoriesList;
+
+  const scenariosUrls: UrlDefinition[] = rolePlayScenarios.map((item) => ({
+    path: `scenarios/${item.id}`,
+    priority: "0.7000",
+  }));
+
+  const scenariosCategoriesUrls: UrlDefinition[] = rolePlayCategories
+    .filter((item) => item.categoryId !== scenariosData.allCategory.categoryId)
+    .map((item) => ({
+      path: `scenarios?category=${item.categoryId}`,
+      priority: "0.5000",
+    }));
+
+  const blogs = getBlogs("en");
+  const blogsItems = blogs.blogs;
+  const blogsCategories = blogs.categoriesList;
+  const blogsUrls: UrlDefinition[] = blogsItems.map((item) => ({
+    path: `blog/${item.id}`,
+    priority: "0.6000",
+  }));
+  const blogsCategoriesUrls: UrlDefinition[] = blogsCategories
+    .filter((item) => item.categoryId !== blogs.allCategory.categoryId)
+    .map((item) => ({
+      path: `blog?category=${item.categoryId}`,
+      priority: "0.5000",
+    }));
+
+  const quizUrls: UrlDefinition[] = supportedLanguages
+    .filter((lang) => lang !== "en")
+    .map((lang) => ({
+      path: `quiz?learn=${lang}`,
+      priority: "0.8000",
+    }));
+
+  const urls: UrlDefinition[] = [
+    {
+      path: "",
+      priority: "1.0000",
+    },
+    {
+      path: "pricing",
+      priority: "0.9000",
+    },
+    {
+      path: "quiz",
+      priority: "0.9000",
+    },
+    {
+      path: "scenarios",
+      priority: "0.8000",
+    },
+
+    {
+      path: "blog",
+      priority: "0.8000",
+    },
+
+    {
+      path: "contacts",
+      priority: "0.6000",
+    },
+    {
+      path: "terms",
+      priority: "0.6000",
+    },
+    {
+      path: "privacy",
+      priority: "0.6000",
+    },
+
+    {
+      path: "cookies",
+      priority: "0.6000",
+    },
+
+    {
+      path: "practice",
+      priority: "0.6000",
+    },
+
+    ...localeLinks,
+    ...scenariosUrls,
+    ...scenariosCategoriesUrls,
+    ...blogsUrls,
+    ...blogsCategoriesUrls,
+    ...quizUrls,
+  ];
+
+  const textResponse = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${urls.map(generateUrl).join("\n")}
+</urlset>
+  `;
+
+  return textResponse;
+}
