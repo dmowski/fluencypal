0678961dbe93788de01c73e3325d4f8af65f9aae
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-01 02:49:20 +0100

    Refactor GameBattle interface to improve structure and readability

--- src/features/Game/Battle/types.ts
+++ src/features/Game/Battle/types.ts
@@ -1,13 +1,34 @@
+export interface GameBattleAnswer {
+  userId: string;
+  questionId: string;
+
+  answer: string;
+
+  updatedAtIso: string;
+}
+
 export interface GameBattle {
   battleId: string;
 
+  authorUserId: string;
   usersIds: string[];
+
   approvedUsersIds: string[];
   rejectedUsersIds: string[];
 
   createdAtIso: string;
+
+  betPoints: number;
+
+  questionsIds: string[];
+
+  answers: GameBattleAnswer[];
+  submittedUsersIds: string[];
+
+  winnerUserId: string | null;
 }
 
+// client side only
 export interface BattleQuestion {
   topic: string;
   description: string;
