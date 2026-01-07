85b9ac6d9376f3bee3323ac01b84751134f02a96
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-07 23:06:38 +0100

    Update subscription prices to reflect new rates for monthly and daily plans

--- src/common/subscription.ts
+++ src/common/subscription.ts
@@ -1,6 +1,6 @@
-export const PRICE_PER_MONTH_USD = 10;
+export const PRICE_PER_MONTH_USD = 11;
 
-export const PRICE_PER_DAY_USD = 1;
+export const PRICE_PER_DAY_USD = 0.99;
 
 export const TRIAL_DAYS = 0;
 
