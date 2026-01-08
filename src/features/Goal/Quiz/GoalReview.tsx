21d418946793a7f2bbb3084214588f29038e3a1b
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-08 11:03:23 +0100

    lang

--- src/features/Goal/Quiz/GoalReview.tsx
+++ src/features/Goal/Quiz/GoalReview.tsx
@@ -9,6 +9,7 @@ import { cardColors, modeCardProps } from "@/features/Plan/data";
 import { LoadingShapes } from "@/features/uiKit/Loading/LoadingShapes";
 import { FooterButton } from "../../Survey/FooterButton";
 import { useAuth } from "@/features/Auth/useAuth";
+import { Avatar } from "@/features/Game/Avatar";
 
 export const GoalReview = ({
   onClick,
@@ -25,6 +26,9 @@ export const GoalReview = ({
 }) => {
   const { i18n } = useLingui();
   const sizes = useWindowSizes();
+  const auth = useAuth();
+
+  const activeImageUrl = auth.userInfo?.photoURL || "/avatar/map.webp";
 
   const modeLabels: Record<PlanElementMode, string> = {
     conversation: i18n._(`Conversation`),
@@ -33,7 +37,6 @@ export const GoalReview = ({
     rule: i18n._(`Rule`),
   };
 
-  const auth = useAuth();
   const username = auth.userInfo?.displayName;
 
   return (
@@ -51,45 +54,43 @@ export const GoalReview = ({
           padding: "0 10px",
           minHeight: `calc(100dvh - ${sizes.topOffset} - ${sizes.bottomOffset} - 190px)`,
           //backgroundColor: "rgba(240, 0, 0, 0.1)",
+          paddingTop: "20px",
         }}
       >
-        <img
-          src={"/avatar/map.webp"}
-          style={{
-            width: "190px",
-            height: "190px",
-          }}
-        />
+        <Avatar url={activeImageUrl} avatarSize="90px" />
         <Stack
           sx={{
             alignItems: "center",
             width: "100%",
-            gap: "30px",
+            gap: "20px",
           }}
         >
           <Stack>
             <Typography
-              variant="caption"
+              variant="body1"
               align="center"
               sx={{
                 opacity: 0.7,
                 textTransform: "uppercase",
               }}
             >
-              {username ? i18n._(`Plan for`) + " " + username : i18n._(`Your plan for learning`)}
+              {username ? i18n._(`Plan for`) + " " + username : i18n._(`Your practice plan`)}
             </Typography>
+
             <Typography
               variant="h5"
               align="center"
               sx={{
                 fontWeight: 660,
                 lineHeight: "1.2",
+                paddingTop: "25px",
               }}
               className={isLoading ? "loading-shimmer" : ""}
             >
               {isLoading ? i18n._("Loading..") : goalData?.title}
             </Typography>
           </Stack>
+
           <Stack
             sx={{
               width: "100%",
