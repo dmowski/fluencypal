d11bab6b2092e02a20509d00d741f63aed24acdc
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-10 18:22:44 +0100

    feat: enhance example display in WelcomeChatMessage component with improved styling

--- src/features/Goal/Quiz/WelcomeChatMessage.tsx
+++ src/features/Goal/Quiz/WelcomeChatMessage.tsx
@@ -30,27 +30,35 @@ const WelcomeChatMessageComponent = (props: WelcomeChatMessageProps) => {
   const exampleToRecord = props.exampleToRecord ? (
     <Stack
       sx={{
-        flexDirection: "row",
         alignItems: "center",
         padding: "15px 15px",
-        borderRadius: "8px",
-        gap: "12px",
+        borderRadius: "15px",
+        gap: "15px",
         backgroundColor: "rgba(99, 177, 135, 0.1)",
-        display: "grid",
-        gridTemplateColumns: "auto 1fr",
       }}
     >
       <Stack
         sx={{
-          background: "linear-gradient(45deg, rgba(99, 177, 135, 1) 0%, #7bd5a1 100%)",
-          height: "40px",
-          width: "40px",
-          borderRadius: "50%",
+          flexDirection: "row",
+          width: "100%",
           alignItems: "center",
-          justifyContent: "center",
+          gridTemplateColumns: "auto 1fr",
+          gap: "15px",
         }}
       >
-        <PawPrint color="#fff" size={"21px"} strokeWidth={"2px"} />
+        <Stack
+          sx={{
+            background: "linear-gradient(45deg, rgba(99, 177, 135, 1) 0%, #7bd5a1 100%)",
+            height: "40px",
+            width: "40px",
+            borderRadius: "50%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <PawPrint color="#fff" size={"21px"} strokeWidth={"2px"} />
+        </Stack>
+        <Typography variant="h6">{i18n._(`Example:`)}</Typography>
       </Stack>
 
       <Stack
@@ -58,14 +66,6 @@ const WelcomeChatMessageComponent = (props: WelcomeChatMessageProps) => {
           gap: "2px",
         }}
       >
-        <Typography
-          sx={{
-            opacity: 0.7,
-          }}
-          variant="body2"
-        >
-          {i18n._(`Example:`)}
-        </Typography>
         <Typography>{props.exampleToRecord}</Typography>
         <Typography
           sx={{
