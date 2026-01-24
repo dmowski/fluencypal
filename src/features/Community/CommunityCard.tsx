ca7474a832e51db22f0884d2077b36dea8dec35b
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-24 22:46:35 +0100

    ref

---
+++ src/features/Community/CommunityCard.tsx
@@ -0,0 +1,58 @@
+import { Badge, Stack, Typography } from '@mui/material';
+
+export const CommunityCard = ({
+  title,
+  onClick,
+  badgeNumber,
+  icon,
+}: {
+  title: string;
+  onClick: () => void;
+  badgeNumber?: number;
+  icon: React.ReactNode;
+}) => {
+  return (
+    <Stack
+      component={'button'}
+      onClick={onClick}
+      sx={{
+        gap: '15px',
+        padding: '30px 15px',
+        textAlign: 'left',
+        color: 'inherit',
+        background: 'rgba(255, 255, 255, 0.05)',
+        border: '1px solid rgba(255, 255, 255, 0.1)',
+
+        height: '100%',
+        width: '100%',
+        borderRadius: '15px',
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ':hover': {
+          background: 'rgba(255, 255, 255, 0.1)',
+        },
+      }}
+    >
+      <Badge color="error" badgeContent={badgeNumber ?? 0}>
+        <Stack
+          sx={{
+            padding: '5px',
+          }}
+        >
+          {icon}
+        </Stack>
+      </Badge>
+      <Typography
+        sx={{
+          fontSize: '15px',
+          textTransform: 'uppercase',
+          opacity: 0.8,
+        }}
+      >
+        {title}
+      </Typography>
+    </Stack>
+  );
+};
--- src/features/Community/CommunityDashboard.tsx
+++ src/features/Community/CommunityDashboard.tsx
@@ -1,11 +1,95 @@
-import { Stack } from '@mui/material';
+'use client';
+
+import { Badge, Stack, Typography } from '@mui/material';
 import { NavigationBar } from '../Navigation/NavigationBar';
+import {
+  Crown,
+  MessageCircle,
+  MessagesSquare,
+  ShieldQuestionMark,
+  Swords,
+  UsersRound,
+} from 'lucide-react';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import { useUrlState } from '../Url/useUrlParam';
+import { CommunityCard } from './CommunityCard';
+
+type Page =
+  | 'chat'
+  | 'game'
+  | 'dm'
+  | 'rate'
+  | 'debates'
+  | 'daily-questions'
+  | 'tech-support'
+  | 'leaderboards';
 
 export const CommunityDashboard = () => {
+  const iconFontSize = '40px';
+
+  const [activePage, setActivePage] = useUrlState<Page | ''>('section', '', false);
+
   return (
     <>
       <NavigationBar />
-      <Stack></Stack>
+      <Stack
+        sx={{
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <Stack
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            gap: '20px',
+            width: '100%',
+            maxWidth: '680px',
+          }}
+        >
+          <CommunityCard
+            title="Community Chat"
+            onClick={() => setActivePage('chat')}
+            badgeNumber={5}
+            icon={<MessagesSquare size={iconFontSize} />}
+          />
+          <CommunityCard
+            title="Game"
+            onClick={() => setActivePage('game')}
+            badgeNumber={0}
+            icon={<Swords size={iconFontSize} />}
+          />
+          <CommunityCard
+            title="Private Chat"
+            onClick={() => setActivePage('dm')}
+            badgeNumber={1}
+            icon={<MessageCircle size={iconFontSize} />}
+          />
+          <CommunityCard
+            title="Debates"
+            onClick={() => setActivePage('debates')}
+            icon={<UsersRound size={iconFontSize} />}
+          />
+          <CommunityCard
+            title="Daily Questions"
+            onClick={() => setActivePage('daily-questions')}
+            icon={<PsychologyIcon sx={{ fontSize: '60px' }} />}
+          />
+
+          <CommunityCard
+            title="Tech Support"
+            onClick={() => setActivePage('tech-support')}
+            icon={<SupportAgentIcon sx={{ fontSize: '60px' }} />}
+          />
+
+          <CommunityCard
+            title="Leaderboards"
+            onClick={() => setActivePage('leaderboards')}
+            icon={<Crown size={iconFontSize} />}
+          />
+        </Stack>
+      </Stack>
     </>
   );
 };
