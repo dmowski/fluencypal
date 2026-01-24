f11fb92e5154c7092ae899cc18448e2fc169e17c
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-24 23:35:42 +0100

    Refactor CommunityDashboard to integrate CommunityPageRouter and update CommunityCard components

--- src/features/Community/CommunityDashboard.tsx
+++ src/features/Community/CommunityDashboard.tsx
@@ -11,24 +11,20 @@ import {
   UsersRound,
 } from 'lucide-react';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
-import PsychologyIcon from '@mui/icons-material/Psychology';
 import { useUrlState } from '../Url/useUrlParam';
 import { CommunityCard } from './CommunityCard';
-
-type Page =
-  | 'chat'
-  | 'game'
-  | 'dm'
-  | 'rate'
-  | 'debates'
-  | 'daily-questions'
-  | 'tech-support'
-  | 'leaderboards';
+import { CommunityPageRouter } from './CommunityPage';
+import { CommunityPage } from './types';
+import { useChatList } from '../Chat/useChatList';
+import { useLingui } from '@lingui/react';
 
 export const CommunityDashboard = () => {
   const iconFontSize = '40px';
 
-  const [activePage, setActivePage] = useUrlState<Page | ''>('section', '', false);
+  const chatList = useChatList();
+  const { i18n } = useLingui();
+
+  const [activePage, setActivePage] = useUrlState<CommunityPage | ''>('section', '', false);
 
   return (
     <>
@@ -39,63 +35,75 @@ export const CommunityDashboard = () => {
           width: '100%',
         }}
       >
-        <Stack
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr 1fr',
-            paddingBottom: '100px',
-            gap: '20px',
-            width: '100%',
-            maxWidth: '680px',
-            '@media (max-width: 600px)': {
-              gridTemplateColumns: '1fr 1fr',
-              paddingTop: '20px',
-            },
-            '@media (max-width: 400px)': {
-              gridTemplateColumns: '1fr',
-            },
-          }}
-        >
-          <CommunityCard
-            title="Community Chat"
-            onClick={() => setActivePage('chat')}
-            badgeNumber={5}
-            icon={<MessagesSquare size={iconFontSize} />}
-          />
-          <CommunityCard
-            title="Game"
-            onClick={() => setActivePage('game')}
-            badgeNumber={0}
-            icon={<Swords size={iconFontSize} />}
-          />
-          <CommunityCard
-            title="Private Chat"
-            onClick={() => setActivePage('dm')}
-            badgeNumber={1}
-            icon={<MessageCircle size={iconFontSize} />}
-          />
-          <CommunityCard
-            title="Debates"
-            onClick={() => setActivePage('debates')}
-            icon={<UsersRound size={iconFontSize} />}
-          />
-          <CommunityCard
-            title="Daily Questions"
-            onClick={() => setActivePage('daily-questions')}
-            icon={<PsychologyIcon sx={{ fontSize: '60px' }} />}
-          />
+        <Stack sx={{ width: '100%', maxWidth: '680px' }}>
+          {activePage ? (
+            <CommunityPageRouter activePage={activePage} onClose={() => setActivePage('')} />
+          ) : (
+            <Stack sx={{ padding: '20px', gap: '20px' }}>
+              <Typography variant="body2">{i18n._('Community Hub')}</Typography>
 
-          <CommunityCard
-            title="Tech Support"
-            onClick={() => setActivePage('tech-support')}
-            icon={<SupportAgentIcon sx={{ fontSize: '60px' }} />}
-          />
+              <Stack
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: '1fr 1fr 1fr',
+                  paddingBottom: '100px',
+                  gap: '20px',
 
-          <CommunityCard
-            title="Leaderboards"
-            onClick={() => setActivePage('leaderboards')}
-            icon={<Crown size={iconFontSize} />}
-          />
+                  '@media (max-width: 600px)': {
+                    gridTemplateColumns: '1fr 1fr',
+                    paddingTop: '20px',
+                  },
+                  '@media (max-width: 400px)': {
+                    gridTemplateColumns: '1fr',
+                  },
+                }}
+              >
+                <CommunityCard
+                  title="Community Chat"
+                  onClick={() => setActivePage('chat')}
+                  badgeNumber={chatList.unreadCountGlobal}
+                  icon={<MessagesSquare size={iconFontSize} />}
+                />
+                <CommunityCard
+                  title="Game"
+                  onClick={() => setActivePage('game')}
+                  badgeNumber={0}
+                  icon={<Swords size={iconFontSize} />}
+                />
+
+                <CommunityCard
+                  title="Tech Support"
+                  onClick={() => setActivePage('tech-support')}
+                  icon={<SupportAgentIcon sx={{ fontSize: '60px' }} />}
+                />
+                <CommunityCard
+                  title="Private Chat"
+                  onClick={() => setActivePage('dm')}
+                  badgeNumber={chatList.myUnreadCount}
+                  icon={<MessageCircle size={iconFontSize} />}
+                />
+                <CommunityCard
+                  title="Debates"
+                  onClick={() => setActivePage('debates')}
+                  icon={<UsersRound size={iconFontSize} />}
+                />
+
+                {/*
+              <CommunityCard
+                title="Daily Questions"
+                onClick={() => setActivePage('daily-questions')}
+                icon={<PsychologyIcon sx={{ fontSize: '60px' }} />}
+              />
+              */}
+
+                <CommunityCard
+                  title="Leaderboards"
+                  onClick={() => setActivePage('leaderboards')}
+                  icon={<Crown size={iconFontSize} />}
+                />
+              </Stack>
+            </Stack>
+          )}
         </Stack>
       </Stack>
     </>
