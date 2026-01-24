3616d3fb34bf1480776e65052888b0c85701825c
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-01-24 23:35:54 +0100

    Fix badge calculation for community navigation item to show only unread messages

--- src/features/Navigation/NavigationBar.tsx
+++ src/features/Navigation/NavigationBar.tsx
@@ -66,7 +66,7 @@ export const NavigationBar: React.FC = () => {
           name: 'community',
           icon: Users,
           title: i18n._('Community'),
-          badge: chatList.myUnreadCount + chatList.unreadCountGlobal,
+          badge: chatList.myUnreadCount,
         },
         {
           name: 'role-play',
