091ced2d9f80b24be12a52ecafba4f0a5da74d69
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-02-05 21:40:43 +0100

    ref

--- src/features/Usage/PaymentModal.tsx
+++ src/features/Usage/PaymentModal.tsx
@@ -35,6 +35,7 @@ import { FaqHours } from './HoursPaymentModal/FaqHours';
 import { BalanceHeader } from './HoursPaymentModal/BalanceHeader';
 import { PriceContact } from './HoursPaymentModal/PriceContact';
 import { HourCard } from './HoursPaymentModal/HourCard';
+import { BalanceContent } from './HoursPaymentModal/BalanceContent';
 
 export const PaymentModal = () => {
   const usage = useUsage();
@@ -111,68 +112,7 @@ export const PaymentModal = () => {
         isOpen={true && auth.isAuthorized}
         onClose={() => usage.togglePaymentModal(false)}
       >
-        <Stack
-          sx={{
-            width: '100%',
-            maxWidth: '700px',
-            gap: '40px',
-          }}
-        >
-          <BalanceHeader />
-
-          <Stack
-            sx={{
-              gap: '10px',
-            }}
-          >
-            <Typography variant="body2">{i18n._('Buy more hours. Why not?')}</Typography>
-            <Stack
-              sx={{
-                width: '100%',
-                gap: '20px',
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr',
-                '@media (max-width: 600px)': {
-                  gridTemplateColumns: '1fr',
-                  gap: '45px',
-                },
-              }}
-            >
-              <HourCard
-                onClick={() => onSelectHourPackage(1)}
-                label={i18n._('1 hour')}
-                content={currency.convertUsdToCurrency(pricePerHourUsd)}
-                buttonTitle={i18n._('Pay')}
-                isRecommended={true}
-                footnote={i18n._('Good for trying the service')}
-              />
-              <HourCard
-                onClick={() => onSelectHourPackage(3)}
-                label={i18n._('3 hours')}
-                content={currency.convertUsdToCurrency(pricePerHourUsd * 3)}
-                buttonTitle={i18n._('Pay')}
-                footnote={i18n._('If you feel it')}
-              />
-              <HourCard
-                onClick={() => onSelectHourPackage(5)}
-                label={i18n._('5 hours')}
-                content={currency.convertUsdToCurrency(pricePerHourUsd * 5)}
-                buttonTitle={i18n._('Pay')}
-                footnote={i18n._('My appreciation')}
-              />
-            </Stack>
-          </Stack>
-
-          <Stack
-            sx={{
-              paddingTop: '20px',
-              gap: '35px',
-            }}
-          >
-            <FaqHours />
-            <PriceContact />
-          </Stack>
-        </Stack>
+        <BalanceContent onSelectHourPackage={onSelectHourPackage} />
       </CustomModal>
     );
   }
