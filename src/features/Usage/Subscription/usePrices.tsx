2ef925ab112cec91e95580e006ca19ad3780a4b6
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-02-10 16:21:42 +0100

    Refactor subscription pricing structure to define prices for all durations

--- src/features/Usage/Subscription/usePrices.tsx
+++ src/features/Usage/Subscription/usePrices.tsx
@@ -1,37 +1,42 @@
-import { PRICE_PER_MONTH_USD, PRICE_PER_DAY_USD } from '@/common/subscription';
+import {
+  PRICE_PER_MONTH_USD,
+  PRICE_PER_DAY_USD,
+  PRICE_PER_YEAR_USD,
+  PRICE_PER_WEEK_USD,
+} from '@/common/subscription';
 import { useCurrency } from '@/features/User/useCurrency';
 import { SubscriptionDuration, PriceInfo } from './types';
 import dayjs from 'dayjs';
 
 export const usePrices = () => {
-  const PRICE_PER_YEAR_USD = PRICE_PER_MONTH_USD * 12;
-  const PRICE_PER_WEEK_USD = PRICE_PER_DAY_USD * 7;
-
   const currency = useCurrency();
   const prices: Record<SubscriptionDuration, PriceInfo> = {
-    month: {
-      usdPrice: PRICE_PER_MONTH_USD,
-      localPrice: currency.convertUsdToCurrency(PRICE_PER_MONTH_USD),
-      currency: currency.currency,
-      expiringDateIso: dayjs().add(30, 'day').toISOString(),
-    },
     day: {
       usdPrice: PRICE_PER_DAY_USD,
       localPrice: currency.convertUsdToCurrency(PRICE_PER_DAY_USD),
       currency: currency.currency,
       expiringDateIso: dayjs().add(1, 'day').toISOString(),
     },
-    year: {
-      usdPrice: PRICE_PER_YEAR_USD,
-      localPrice: currency.convertUsdToCurrency(PRICE_PER_YEAR_USD),
-      currency: currency.currency,
-      expiringDateIso: dayjs().add(365, 'day').toISOString(),
-    },
+
     week: {
       usdPrice: PRICE_PER_WEEK_USD,
       localPrice: currency.convertUsdToCurrency(PRICE_PER_WEEK_USD),
       currency: currency.currency,
       expiringDateIso: dayjs().add(7, 'day').toISOString(),
+    },
+
+    month: {
+      usdPrice: PRICE_PER_MONTH_USD,
+      localPrice: currency.convertUsdToCurrency(PRICE_PER_MONTH_USD),
+      currency: currency.currency,
+      expiringDateIso: dayjs().add(30, 'day').toISOString(),
+    },
+
+    year: {
+      usdPrice: PRICE_PER_YEAR_USD,
+      localPrice: currency.convertUsdToCurrency(PRICE_PER_YEAR_USD),
+      currency: currency.currency,
+      expiringDateIso: dayjs().add(365, 'day').toISOString(),
     },
   };
 
