09c429131e266a0a73c9d4933f15fe8110c32c90
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-02-12 21:18:36 +0100

    ref

--- src/features/Usage/Subscription/ActivePlanSelector.tsx
+++ src/features/Usage/Subscription/ActivePlanSelector.tsx
@@ -1,9 +1,9 @@
 import { useLingui } from '@lingui/react';
-import { Stack, Typography } from '@mui/material';
+import { Button, ButtonGroup, Stack } from '@mui/material';
 import { SubscriptionCard } from './SubscriptionCard';
 import { SubscriptionDuration } from './types';
 import { usePrices } from './usePrices';
-import dayjs from 'dayjs';
+import { useState } from 'react';
 
 export const ActivePlanSelector = ({
   onSelectDuration,
@@ -13,13 +13,41 @@ export const ActivePlanSelector = ({
   const { i18n } = useLingui();
 
   const prices = usePrices();
+  const [selectedDuration, setSelectedDuration] = useState<SubscriptionDuration>('month');
 
   return (
     <Stack
       sx={{
         width: '100%',
+        gap: '20px',
       }}
     >
+      <ButtonGroup>
+        <Button
+          onClick={() => {
+            setSelectedDuration('day');
+          }}
+          variant={selectedDuration === 'day' ? 'contained' : 'outlined'}
+        >
+          {i18n._('1 day')}
+        </Button>
+        <Button
+          onClick={() => {
+            setSelectedDuration('week');
+          }}
+          variant={selectedDuration === 'week' ? 'contained' : 'outlined'}
+        >
+          {i18n._('1 week')}
+        </Button>
+        <Button
+          onClick={() => {
+            setSelectedDuration('month');
+          }}
+          variant={selectedDuration === 'month' ? 'contained' : 'outlined'}
+        >
+          {i18n._('1 month')}
+        </Button>
+      </ButtonGroup>
       <Stack
         sx={{
           width: '100%',
@@ -34,30 +62,35 @@ export const ActivePlanSelector = ({
           },
         }}
       >
-        <SubscriptionCard
-          title={i18n._('1 day')}
-          onPay={() => onSelectDuration('day')}
-          priceInUsd={prices.subscriptionPrices.day.usdPrice}
-          duration={'day'}
-          isSuggested
-          expiringDateIso={prices.subscriptionPrices.day.expiringDateIso}
-        />
+        {selectedDuration === 'day' && (
+          <SubscriptionCard
+            title={i18n._('1 day')}
+            onPay={() => onSelectDuration('day')}
+            priceInUsd={prices.subscriptionPrices.day.usdPrice}
+            duration={'day'}
+            expiringDateIso={prices.subscriptionPrices.day.expiringDateIso}
+          />
+        )}
 
-        <SubscriptionCard
-          title={i18n._('1 week')}
-          onPay={() => onSelectDuration('week')}
-          priceInUsd={prices.subscriptionPrices.week.usdPrice}
-          duration={'week'}
-          expiringDateIso={prices.subscriptionPrices.week.expiringDateIso}
-        />
+        {selectedDuration === 'week' && (
+          <SubscriptionCard
+            title={i18n._('1 week')}
+            onPay={() => onSelectDuration('week')}
+            priceInUsd={prices.subscriptionPrices.week.usdPrice}
+            duration={'week'}
+            expiringDateIso={prices.subscriptionPrices.week.expiringDateIso}
+          />
+        )}
 
-        <SubscriptionCard
-          title={i18n._('1 month')}
-          onPay={() => onSelectDuration('month')}
-          priceInUsd={prices.subscriptionPrices.month.usdPrice}
-          duration={'month'}
-          expiringDateIso={prices.subscriptionPrices.month.expiringDateIso}
-        />
+        {selectedDuration === 'month' && (
+          <SubscriptionCard
+            title={i18n._('1 month')}
+            onPay={() => onSelectDuration('month')}
+            priceInUsd={prices.subscriptionPrices.month.usdPrice}
+            duration={'month'}
+            expiringDateIso={prices.subscriptionPrices.month.expiringDateIso}
+          />
+        )}
         {/*
         <SubscriptionCard
           title={i18n._('1 year')}
