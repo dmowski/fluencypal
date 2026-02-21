5c7334080b825dff99cdd8627585aedc7f9e496f
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-02-21 17:54:53 +0100

    feat: update leadingSilenceDurationSec to improve silence detection accuracy

--- helperProjects/trimAudios/src/core/ffmpegConfig.ts
+++ helperProjects/trimAudios/src/core/ffmpegConfig.ts
@@ -2,7 +2,7 @@ export const ffmpegSilenceCheckConfig = {
   inputDirectoryName: "loadedData",
   outputDirectoryName: "processedData",
   maxFilesToCheck: 5,
-  leadingSilenceDurationSec: 0.05,
+  leadingSilenceDurationSec: 0.001,
   trailingSilenceKeepSec: 0.1,
   silenceNoiseThreshold: "-35dB",
   leadingSilenceEpsilonSec: 0.001,
