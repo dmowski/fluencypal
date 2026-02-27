e3539cd6be63f98cab61a90dd9969304d1522858
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-02-27 12:24:48 +0100

    Refactor Stories component layout and enhance story selection; add button for navigating to stories and update styling for better responsiveness

--- web/src/features/Sentence/Stories.tsx
+++ web/src/features/Sentence/Stories.tsx
@@ -1,5 +1,5 @@
 import { useLingui } from '@lingui/react';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { useEffect, useMemo, useState } from 'react';
 import { useConversationAudio } from '../Audio/useConversationAudio';
@@ -15,10 +15,10 @@ import { StoryModal } from './StoryModal';
 import { uniq } from '@/libs/uniq';
 import { shuffleArray } from '@/libs/array';
 import { useStories } from './useStories';
+import { ArrowRight } from 'lucide-react';
 
 export const Stories = () => {
   const { i18n } = useLingui();
-  const [selectedImageImageId, setSelectedImageId] = useUrlState('storyImage', '', false);
 
   const auth = useAuth();
 
@@ -26,7 +26,6 @@ export const Stories = () => {
   const audio = useConversationAudio();
 
   const closeStory = () => {
-    setSelectedImageId('');
     audio.music.stop();
   };
 
@@ -49,7 +48,7 @@ export const Stories = () => {
   };
 
   const onSelectImage = async (imageId: string) => {
-    setSelectedImageId(imageId);
+    stories.openStory(imageId);
     await audio.initAudio();
     const story = stories.openStory(imageId);
     playStoryAudio(story);
@@ -58,76 +57,77 @@ export const Stories = () => {
   return (
     <Stack
       sx={{
-        alignItems: 'flex-start',
-        gap: '0px',
+        alignItems: 'center',
+        gap: '40px',
         marginTop: '20px',
 
         width: '100%',
         position: 'relative',
+        flexDirection: 'row',
       }}
     >
       <Stack
         sx={{
-          width: '100%',
-          justifyContent: 'space-between',
-          flexDirection: 'row',
-          padding: '5px 10px 0px 10px',
-          flexWrap: 'wrap',
-        }}
-      >
-        <Typography
-          variant="body2"
-          sx={{
-            opacity: 0.8,
-          }}
-        >
-          {i18n._('Expand vocabulary with stories')}
-        </Typography>
-      </Stack>
-
-      <Stack
-        sx={{
           position: 'relative',
-          width: '99%',
-          overflow: 'hidden',
-          height: 'calc(100% + 20px)',
         }}
       >
-        <Stack
-          sx={{
-            overflowX: 'scroll',
-            paddingBottom: '15px',
-            paddingRight: '10px',
-
-            // Scrollbar styles
-            '&::-webkit-scrollbar': {
-              height: '0px',
-            },
-            '&::-webkit-scrollbar-thumb': {
-              backgroundColor: 'rgba(100, 100, 100, 0.1)',
-              borderRadius: '4px',
-            },
-            '&::-webkit-scrollbar-thumb:hover': {
-              backgroundColor: 'rgba(100, 100, 100, 0.1)',
-            },
-          }}
-        >
+        <Stack sx={{}}>
           <Stack
             sx={{
               flexDirection: 'row',
-              alignItems: 'flex-start',
-              gap: '10px',
-              padding: '6px 15px 20px 7px',
-              width: 'max-content',
-              minHeight: '220px',
+              position: 'relative',
             }}
           >
-            {!stories.selectedStory &&
-              stories.stories.map((story) => {
-                return <StoryPreview key={story.id} onSelectImage={onSelectImage} image={story} />;
-              })}
+            {!stories.selectedStory && stories.stories && (
+              <>
+                <Stack
+                  sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: '20px',
+                    transform: 'rotate(0deg) scale(0.9)',
+                  }}
+                >
+                  <StoryPreview onSelectImage={() => {}} image={stories.stories[0]} />
+                </Stack>
+                <StoryPreview onSelectImage={() => {}} image={stories.stories[1]} />
+                <Stack
+                  sx={{
+                    position: 'absolute',
+
+                    top: 0,
+                    left: '10px',
+                    transform: 'rotate(0deg) scale(0.95)',
+                  }}
+                >
+                  <StoryPreview onSelectImage={() => {}} image={stories.stories[2]} />
+                </Stack>
+              </>
+            )}
           </Stack>
         </Stack>
+      </Stack>
+      <Stack
+        sx={{
+          width: '100%',
+          alignItems: 'flex-start',
+          gap: '15px',
+        }}
+      >
+        <Stack>
+          <Typography
+            variant="h4"
+            sx={{
+              fontWeight: 800,
+            }}
+          >
+            {i18n._('No time to speak?')}
+          </Typography>
+          <Typography>{i18n._('Play stories and listen to them on the go')}</Typography>
+        </Stack>
+        <Button variant="outlined" color="info" endIcon={<ArrowRight />} onClick={onNext}>
+          {i18n._('See stories')}
+        </Button>
       </Stack>
     </Stack>
   );
