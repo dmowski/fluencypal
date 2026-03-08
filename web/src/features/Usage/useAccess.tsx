a3e9378bf3a6b52d0c46f2f2f291c0dcc5cc24f6
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-08 22:40:40 +0100

    Remove unused import of useAuth from useAccess for cleaner code

--- web/src/features/Usage/useAccess.tsx
+++ web/src/features/Usage/useAccess.tsx
@@ -2,13 +2,11 @@ import dayjs from 'dayjs';
 import { useGame } from '../Game/useGame';
 import { useUsage } from './useUsage';
 import { useSettings } from '../Settings/useSettings';
-import { useAuth } from '../Auth/useAuth';
 
 export const useAccess = () => {
   const game = useGame();
   const usage = useUsage();
   const settings = useSettings();
-  const auth = useAuth();
 
   const isParentalConsentNeeded = settings.userSettings?.isParentalConsentNeeded || false;
   const isCreditCardValidated = settings.userSettings?.isCreditCardConfirmed;
