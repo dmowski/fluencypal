adcc1401ad7136157e428a6138f167f560bce3ef
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-20 01:11:35 +0100

    lang

--- web/src/features/Dashboard/PublicChatDashboardCard.tsx
+++ web/src/features/Dashboard/PublicChatDashboardCard.tsx
@@ -1,18 +1,11 @@
-import dayjs from 'dayjs';
-import { useSettings } from '../Settings/useSettings';
-import { dailyQuestions } from '../Game/DailyQuestion/dailyQuestions';
 import { useLingui } from '@lingui/react';
 import { useAccess } from '../Usage/useAccess';
-import { ChatProvider } from '../Chat/useChat';
 import { Stack, Typography } from '@mui/material';
 import { StoreCard } from '../uiKit/Card/StoreCard';
-import { FlatChat } from '../Chat/FlatChat';
-import { SectionHeader } from './CartsHeader';
+
 import { CustomModal } from '../uiKit/Modal/CustomModal';
-import { DailyQuestionBadge } from '../Game/DailyQuestion/DailyQuestionBadge';
 import { useUrlState } from '../Url/useUrlState';
-import { GlobalChatTabs } from '../Chat/GlobalChatTabs';
-import { ChatSection } from '../Chat/ChatSection';
+
 import { ChatPage } from '../Chat/ChatPage';
 
 export const PublicChatDashboardCard = () => {
@@ -57,14 +50,6 @@ export const PublicChatDashboardCard = () => {
               >
                 {i18n._('Community Chat')}
               </Typography>
-              <Typography
-                variant="body1"
-                sx={{
-                  color: 'text.secondary',
-                }}
-              >
-                {i18n._('Join the community chat and connect with other members!')}
-              </Typography>
             </Stack>
 
             <ChatPage type={'public'} />
@@ -83,8 +68,8 @@ export const PublicChatDashboardCard = () => {
           previewImageUrl={
             'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1773964951620-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.jpg'
           }
-          title={i18n._('Global Chat')}
-          subTitle={i18n._('Join the global chat and connect with other members!')}
+          title={i18n._('Community Chat')}
+          subTitle={i18n._('Join the community chat and connect with other members!')}
           items={[]}
           onClick={openAll}
           itemsBackgroundColor={'rgba(32, 32, 32, 1)'}
