501c2bfc57c3694a659e16d4a1c396eff4a8e7ea
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-21 19:22:51 +0100

    ref

--- web/src/features/Dashboard/DailyTasksDashboardCard.tsx
+++ web/src/features/Dashboard/DailyTasksDashboardCard.tsx
@@ -1,8 +1,23 @@
 import { useLingui } from '@lingui/react';
 import { Stack } from '@mui/material';
-import { StoreCard } from '../uiKit/Card/StoreCard';
+import { RowItem, StoreCard } from '../uiKit/Card/StoreCard';
 import { useDailyTasks } from '../Tasks/useDailyTasks';
 import { SectionHeader } from './CartsHeader';
+import { useMemo } from 'react';
+import { DailyTaskType } from '../Tasks/types';
+
+const taskIconMap: Record<DailyTaskType, string> = {
+  'just-talk':
+    'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774036079435-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
+  'goal-lesson':
+    'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035398903-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
+  community:
+    'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035331701-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
+  story:
+    'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035304855-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
+  'daily-question':
+    'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035287672-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
+};
 
 export const DailyTasksDashboardCard = () => {
   const { i18n } = useLingui();
@@ -13,6 +28,25 @@ export const DailyTasksDashboardCard = () => {
     tasks.onStartTask(tasks.todaysActualTasks[0]);
   };
 
+  const items: RowItem[] = useMemo(() => {
+    return tasks.todaysActualTasks.map((taskType) => {
+      const taskInfo = tasks.tasksInfo ? tasks.tasksInfo[taskType] : null;
+      const isCompleted = tasks.todayTaskProgress
+        ? !!tasks.todayTaskProgress.completedTasks[taskType]
+        : false;
+      const image = taskIconMap[taskType];
+      const taskItem: RowItem = {
+        title: taskInfo ? taskInfo.title : taskType,
+        onClick: () => tasks.onStartTask(taskType),
+        subTitle: taskInfo ? taskInfo.label : '',
+        actionButtonTitle: isCompleted ? i18n._('Completed') : i18n._('Start'),
+        imageUrl: image,
+        bgColor: isCompleted ? 'rgba(147, 7, 255, 0.5)' : 'rgba(147, 7, 255, 0.2)',
+      };
+      return taskItem;
+    });
+  }, [tasks.todaysActualTasks, tasks.tasksInfo, tasks.todayTaskProgress]);
+
   return (
     <Stack
       sx={{
@@ -22,14 +56,14 @@ export const DailyTasksDashboardCard = () => {
       <SectionHeader title={i18n._('Daily tasks')} />
       <StoreCard
         textColor={'#fff'}
-        backgroundColor={'#00000065'}
+        backgroundColor={'#9307ff69'}
         previewImageUrl={tasks.previewImageUrl}
         title={tasks.title}
         subTitle={tasks.subTitle}
         badge={tasks.badge}
-        items={[]}
+        items={items}
         onClick={openCard}
-        itemsBackgroundColor={'rgba(32, 32, 32, 1)'}
+        itemsBackgroundColor={'rgba(32, 32, 32, 0.9)'}
         itemsViewMode={'list'}
       />
     </Stack>
