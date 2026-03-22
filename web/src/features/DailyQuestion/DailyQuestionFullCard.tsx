ebd476b3ba5c05c08866ad38848ebe51569a636b
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-22 15:28:30 +0100

    Make badge prop optional in DailyQuestionFullCard component

--- web/src/features/DailyQuestion/DailyQuestionFullCard.tsx
+++ web/src/features/DailyQuestion/DailyQuestionFullCard.tsx
@@ -13,7 +13,7 @@ export const DailyQuestionFullCard = ({
   onClick,
 }: {
   question: DailyQuestion;
-  badge: string;
+  badge?: string;
   onClick?: () => void;
 }) => {
   const previewImageUrl = getDailyQuestionImage(question);
