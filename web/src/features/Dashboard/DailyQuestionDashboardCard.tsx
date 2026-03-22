d8678a4828bba6451f96d1c11d7a9a406ccfebfd
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-22 14:22:13 +0100

    Refactor DailyQuestionDashboardCard to use dailyQuestionsImages from new data module for improved image management

--- web/src/features/Dashboard/DailyQuestionDashboardCard.tsx
+++ web/src/features/Dashboard/DailyQuestionDashboardCard.tsx
@@ -9,19 +9,7 @@ import { StoreCard } from '@/features//uiKit/Card/StoreCard';
 import { FlatChat } from '@/features//Chat/FlatChat';
 import { SectionHeader } from './CartsHeader';
 import { useGlobalModals } from '@/features//Modal/useGlobalModals';
-
-const images: string[] = [
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774036079435-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035398903-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035331701-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035304855-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035287672-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035201036-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035156483-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774035037754-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1774034994467-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-  'https://storage.googleapis.com/dark-lang.firebasestorage.app/uploadedImages%2FMq2HfU3KrXTjNyOpPXqHSPg5izV2%2F1773947976503-Mq2HfU3KrXTjNyOpPXqHSPg5izV2.png',
-];
+import { dailyQuestionsImages } from '../DailyQuestion/data';
 
 export const DailyQuestionDashboardCard = () => {
   const settings = useSettings();
@@ -30,7 +18,7 @@ export const DailyQuestionDashboardCard = () => {
   const questionsKeys = Object.keys(dailyQuestions);
   const questionIndex = daysSinceStart % questionsKeys.length;
 
-  const previewImageUrl = images[questionIndex % images.length];
+  const previewImageUrl = dailyQuestionsImages[questionIndex % dailyQuestionsImages.length];
 
   const todaysQuestion = dailyQuestions[questionsKeys[questionIndex]];
   const { i18n } = useLingui();
