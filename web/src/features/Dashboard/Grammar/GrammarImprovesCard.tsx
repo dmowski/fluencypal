8f8ffa0ba1ff126d0d17c40ac31fd8aab492e21a
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-03-24 18:06:36 +0100

    Fix dependency array in useMemo for GrammarImprovesCardUi to include full grammarPoints object

--- web/src/features/Dashboard/Grammar/GrammarImprovesCard.tsx
+++ web/src/features/Dashboard/Grammar/GrammarImprovesCard.tsx
@@ -64,7 +64,7 @@ export const GrammarImprovesCardUi = () => {
     }
 
     return newItems;
-  }, [grammarPoints.length, titleMap, limit, isLimited]);
+  }, [grammarPoints, titleMap, limit, isLimited]);
 
   return (
     <Stack sx={{ gap: '20px' }}>
