1afb4179f4fbe5ecdfcec6ef292e87e7461266a3
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-04-05 12:38:49 +0200

    feat: add maxDuration constant and schedule for assessment API in Vercel configuration

--- web/src/app/api/assessment/route.ts
+++ web/src/app/api/assessment/route.ts
@@ -1,5 +1,7 @@
 import { processAssessment } from '@/features/ProgressStat/backend/processAssessment';
 
+export const maxDuration = 300;
+
 export async function GET(request: Request) {
   await processAssessment();
   return new Response('Hello, Assessment!', {
