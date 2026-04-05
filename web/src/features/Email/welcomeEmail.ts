46d632b08fa87b78a45c4da4f491d92d9ead214f
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-04-05 18:13:13 +0200

    feat: update welcome email text for a more personal touch

--- web/src/features/Email/welcomeEmail.ts
+++ web/src/features/Email/welcomeEmail.ts
@@ -1,6 +1,13 @@
 export const getWelcomeEmailText = ({}: {}) => {
-  return `Hi. I am the creator of FluencyPal. My goal 
+  return `Hi. My name is Alex. I'm the creator of this app.
 
+FluencyPal isn't very popular right now, and I don't plan to make it as popular as Duolingo or other similar apps.
 
-`;
+So, I have the opportunity to talk to you.
+
+I simply want to create a platform/space where we can feel supported without judgment and be in control of our practice.
+
+So, if you have any questions, suggestions, or if something didn't meet your expectations, just message me, and I'll do my best to fix it.
+
+Thank you for registering and welcome!`;
 };
