abbf0fa27ed1c13dda0d24b6231d886d5da49297
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-04-07 01:22:08 +0200

    fix: update currency formatting to display without decimal places

--- web/src/features/User/useCurrency.tsx
+++ web/src/features/User/useCurrency.tsx
@@ -109,7 +109,7 @@ export const useCurrency = () => {
     const formattedAmount = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency,
-      maximumFractionDigits: 2,
+      maximumFractionDigits: 0,
     }).format(convertedAmount);
 
     return formattedAmount;
