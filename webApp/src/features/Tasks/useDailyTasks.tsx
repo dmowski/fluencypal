4ad8a98f39eea55cab3715fb07a9782b046c1e25
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-04-23 17:38:35 +0200

    fix: update daily plans to remove unnecessary task from the list

--- webApp/src/features/Tasks/useDailyTasks.tsx
+++ webApp/src/features/Tasks/useDailyTasks.tsx
@@ -117,7 +117,7 @@ function useProvideDailyTasks(): DailyTaskApi {
 
   const dailyPlans: DayTasksMeta[] = [
     {
-      tasks: ['just-talk', 'goal-lesson'],
+      tasks: ['just-talk'],
       title: i18n._('Build a steady learning habit'),
       subTitle: i18n._('Start with today’s tasks and keep a gentle pace.'),
       imageUrl: previewImageUrl,
