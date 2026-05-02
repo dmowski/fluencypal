0f919d7ec0007c4f234eb48cae90fdfd523c4629
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-05-02 16:45:58 +0200

    feat: remove unused splitParagraphsIntoPages function and add textAlign constant for consistency

--- webApp/src/features/Reader/utils/splitParagraphsIntoPages.tsx
+++ webApp/src/features/Reader/utils/splitParagraphsIntoPages.tsx
@@ -2,6 +2,8 @@ import { getHash } from '@/libs/hash';
 import { splitWords } from '../../Sentence/TextConstructor/textConstructor.utils';
 import { BookParagraph } from '../model/types';
 
+const textAlign = 'justify';
+
 interface UiSettings {
   fontSize: number;
   lineHeight: number;
@@ -56,33 +58,6 @@ export const splitIntoPages = ({
   }
 
   splitIntoPagesCache.set(hash, pages);
-
-  return pages;
-};
-
-export const splitParagraphsIntoPages = (
-  paragraphs: string[],
-  pageSizeChars: number,
-): string[][] => {
-  const [firstParagraph, ...restParagraphs] = paragraphs;
-
-  const pages: string[][] = [firstParagraph ? [firstParagraph] : []];
-  let currentPage: string[] = pages[0];
-  let currentPageCharCount = firstParagraph ? firstParagraph.length : 0;
-
-  restParagraphs.forEach((paragraph) => {
-    if (currentPageCharCount + paragraph.length > pageSizeChars) {
-      pages.push(currentPage);
-      currentPage = [];
-      currentPageCharCount = 0;
-    }
-    currentPage.push(paragraph);
-    currentPageCharCount += paragraph.length;
-  });
-
-  if (currentPage.length > 0) {
-    pages.push(currentPage);
-  }
 
   return pages;
 };
