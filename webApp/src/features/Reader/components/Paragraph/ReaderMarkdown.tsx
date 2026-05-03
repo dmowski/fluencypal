81cc4ab4cff22bef7fdb1586ed7e04d95cd37f1a
Alexandr Dmowski <dmowski.alex@gmail.com>
2026-05-03 10:51:28 +0200

    feat: add Markdown component with custom rendering for various HTML elements

---
+++ webApp/src/features/Reader/components/Paragraph/ReaderMarkdown.tsx
@@ -0,0 +1,174 @@
+'use client';
+
+import { Checkbox, Link, Stack, Typography } from '@mui/material';
+import { MarkdownToJSX, default as MarkdownTool } from 'markdown-to-jsx';
+import React from 'react';
+
+export interface MarkdownProps {
+  children: string;
+  onWordClick?: (word: string, element: HTMLElement) => void;
+}
+
+const processStringChild = (child: string, index: number) => {
+  const words = child.split(' ');
+  return words.map((word, wordIndex) => (
+    <span key={`${index}-${wordIndex}`}>
+      <span className="conversation-word">{word}</span>
+      {wordIndex < words.length - 1 ? ' ' : ''}
+    </span>
+  ));
+};
+
+const wrapChildrenWithTranslateWrapper = (children: React.ReactNode): React.ReactNode => {
+  const isChildrenIsArray = Array.isArray(children);
+  if (!isChildrenIsArray) {
+    const isString = typeof children === 'string';
+    if (isString) {
+      return processStringChild(children, 0);
+    }
+
+    return children;
+  }
+
+  const processedChildren = children.map((child, index) => {
+    if (typeof child === 'string') {
+      return processStringChild(child, index);
+    }
+    return child;
+  });
+
+  return processedChildren;
+};
+
+const markdownComponents: MarkdownToJSX.Overrides = {
+  h1: ({ children }) => <Typography variant="h1">{children}</Typography>,
+  h2: ({ children }) => (
+    <Typography
+      variant="h2"
+      sx={{
+        paddingTop: '20px',
+      }}
+    >
+      {children}
+    </Typography>
+  ),
+  h3: ({ children }) => (
+    <Typography
+      variant="h4"
+      component={'h3'}
+      sx={{
+        paddingTop: '20px',
+      }}
+    >
+      {children}
+    </Typography>
+  ),
+  h4: ({ children }) => (
+    <Typography
+      variant="h5"
+      component={'h4'}
+      sx={{
+        paddingTop: '20px',
+      }}
+    >
+      {children}
+    </Typography>
+  ),
+  h5: ({ children }) => <Typography variant="h5">{children}</Typography>,
+  h6: ({ children }) => <Typography variant="h6">{children}</Typography>,
+
+  p: ({ children }) => <span>{wrapChildrenWithTranslateWrapper(children)}</span>,
+  span: ({ children }) => <span>{wrapChildrenWithTranslateWrapper(children)}</span>,
+
+  a: ({ href, children }) => (
+    <Link href={href} target="_blank">
+      {children}
+    </Link>
+  ),
+  ul: ({ children }) => (
+    <ul
+      style={{
+        padding: '0 0 0 20px',
+        margin: '5px 0',
+      }}
+    >
+      {children}
+    </ul>
+  ),
+  ol: ({ children }) => (
+    <ol
+      style={{
+        padding: '0 0 0 20px',
+        margin: '5px 0',
+      }}
+    >
+      {children}
+    </ol>
+  ),
+  li: ({ children }) => <Typography component={'li'}>{children}</Typography>,
+  input: ({ checked }) => (
+    <Checkbox
+      checked={checked}
+      disabled
+      sx={{
+        padding: '0',
+      }}
+    />
+  ),
+  small: ({ children }) => <Typography>{children}</Typography>,
+  em: ({ children }) => <em>{children}</em>,
+  strong: ({ children }) => <strong>{children}</strong>,
+  blockquote: ({ children }) => (
+    <Stack
+      component={'blockquote'}
+      sx={{
+        margin: '10px 10px 10px 0',
+        padding: '5px 10px 5px 15px',
+        borderLeft: '4px solid rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      {children}
+    </Stack>
+  ),
+  pre: ({ children }) => <pre>{children}</pre>,
+  code: ({ children }) => <code>{children}</code>,
+  thead: ({ children }) => <thead>{children}</thead>,
+  tbody: ({ children }) => <tbody>{children}</tbody>,
+  tr: ({ children }) => <tr>{children}</tr>,
+  th: ({ children }) => <th>{children}</th>,
+  td: ({ children }) => <td>{children}</td>,
+  table: ({ children }) => <table>{children}</table>,
+  img: (props) => <img {...props} style={{ maxWidth: '90%' }} />,
+};
+
+export const Markdown: React.FC<MarkdownProps> = ({ children, onWordClick }) => {
+  const styleComponents = markdownComponents;
+
+  return (
+    <Stack
+      sx={{
+        '.conversation-word': onWordClick
+          ? {
+              ':hover': {
+                //
+              },
+            }
+          : {},
+      }}
+      onMouseDown={
+        onWordClick
+          ? (e) => {
+              const target = e.target as HTMLElement;
+              if (target.classList.contains('conversation-word')) {
+                const word = target.textContent || '';
+                const element = target;
+                onWordClick(word.trim(), element);
+              }
+            }
+          : undefined
+      }
+    >
+      <MarkdownTool options={{ overrides: styleComponents }}>{children}</MarkdownTool>
+    </Stack>
+  );
+};
